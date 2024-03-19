bbad6ca2ffef59d8199fcda7c8ae86df0283c998
softmoca <105472863+softmoca@users.noreply.github.com>
2024-03-19 14:42:15 +0900

    Title: n개 중에 m개 뽑기, Time: 133ms, Memory: 25MB, Status: Passed - CodeTree

--- 240319/n개 중에 m개 뽑기/n-choose-m.py
+++ 240319/n개 중에 m개 뽑기/n-choose-m.py
@@ -1,17 +1,12 @@
 def dfs(x,s):
     if x==m:
         for i in range(len(res)):
-            print(res[i])
+            print(res[i],end=' ')
         print()
     else:
         for i in range(s,n+1):
             res[x]=i
             dfs(x+1,i+1)
-
-
 n,m=map(int,input().split())
-
 res=[0]*m
-
-
 dfs(0,1)
