9ca470af4dfab6fabfd6c593263e7f660e53885a
softmoca <105472863+softmoca@users.noreply.github.com>
2024-04-01 23:48:04 +0900

    Title: 길이 단위 변환하기, Time: 114ms, Memory: 8MB, Status: Passed - CodeTree

--- 240401/길이 단위 변환하기/change-length-unit.java
+++ 240401/길이 단위 변환하기/change-length-unit.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        double fit=30.48;
-        double mi=160934;
-        System.out.printf("9.2fit = %.1fcm",9.2*fit);
-        System.out.println();
-        System.out.printf("1.3mi = %.1fcm",1.3*mi);
+        // 변수 선언
+        double a = 9.2;
+	    double b = 1.3;
+
+        // 출력
+        System.out.printf("%.1fft = %.1fcm\n%.1fmi = %.1fcm", a, a*30.48, b, b*160934);
     }
 }
