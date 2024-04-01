a54694b7b69141cd5543c6685c21136bbcf2003b
softmoca <105472863+softmoca@users.noreply.github.com>
2024-04-01 23:52:23 +0900

    Title: 변수 값 교체하기 2, Time: 93ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240401/변수 값 교체하기 2/replacing-variable-values-2.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main (String args[]) {
+        int a = 5, b = 3;
+        System.out.println(a);
+
+        a = b;
+ System.out.println(a);
+ System.out.println(b);
+    }
+}
