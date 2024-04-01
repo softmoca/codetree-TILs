31e4db759fa5123e7e5f6c0d0e4d646759253640
softmoca <105472863+softmoca@users.noreply.github.com>
2024-04-02 00:29:35 +0900

    Title: 문자열 순서 바꾸기, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240402/문자열 순서 바꾸기/change-order-of-strings.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언
+        String a;
+        String b;
+
+        // 입력
+        a = sc.next();
+
+        b = sc.next();
+
+        // 출력
+        System.out.println(b);
+        System.out.println(a);
+        
+
+    }
+}
