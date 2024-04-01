7ba199c07767783815a904b0c8842b49a1de5317
softmoca <105472863+softmoca@users.noreply.github.com>
2024-04-01 23:56:26 +0900

    Title: 정수 복사, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240401/정수 복사/copy-integer.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+
+        // 변수 a의 값을 변수 b에 넣습니다.
+        b = a;
+
+        // 변수 a와 b의 값을 곱합니다.
+        int result = a * b;
+
+        // 결과값을 출력합니다.
+        System.out.printf("%d %d",a,b);
+        System.out.println();
+        System.out.println(result);
+    }
+}
