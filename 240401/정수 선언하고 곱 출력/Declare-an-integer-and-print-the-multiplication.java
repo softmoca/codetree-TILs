76296cfa25c5a4c58c0962b323cc189a30799014
softmoca <105472863+softmoca@users.noreply.github.com>
2024-04-01 23:36:06 +0900

    Title: 정수 선언하고 곱 출력, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240401/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 26;
+        int num2 = 5;
+
+        // 뺄셈 결과를 저장하는 변수
+        int result = num1 * num2;
+
+        // printf() 메서드 사용
+        System.out.printf("%d * %d = %d\n", num1, num2, result);
+        
+
+
+
+    }
+}
