ad7e7111ee36c557a6d87b7d03fe400f5c1104fe
softmoca <105472863+softmoca@users.noreply.github.com>
2024-04-01 23:40:39 +0900

    Title: 변수 출력하기 2, Time: 91ms, Memory: 9MB, Status: Passed - CodeTree

--- 240401/변수 출력하기 2/outputing-variables-2.java
+++ 240401/변수 출력하기 2/outputing-variables-2.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         int a=3;
-        char b="C";
+        char b='C';
         System.out.printf("%c!.....!%d",b,a);
      
      
