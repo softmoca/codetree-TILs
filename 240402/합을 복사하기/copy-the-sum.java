62714ef2ecb51589fd7772804b42a7f398da6050
softmoca <105472863+softmoca@users.noreply.github.com>
2024-04-02 00:00:43 +0900

    Title: 합을 복사하기, Time: 104ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240402/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        // 세 정수의 합을 계산합니다.
+        int sum = a + b + c;
+
+        // 각 변수에 합을 저장합니다.
+        a = sum;
+        b = sum;
+        c = sum;
+
+        // 각 변수의 값을 출력합니다.
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
