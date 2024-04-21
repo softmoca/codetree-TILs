116e62e030a3f21507e5442e8d2e5d43bacd1a93
softmoca <105472863+softmoca@users.noreply.github.com>
2024-04-21 15:50:21 +0900

    Title: 별표 출력하기 3, Time: 147ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240421/별표 출력하기 3/print-star-3.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+      
+        
+       
+        for(int i = 0; i < n; i++) {
+        	for(int k=0; k<2*i;k++) {
+        		System.out.print(" ");
+        	}
+            for(int j = 2*i; j<2*n-1; j++)
+                System.out.print("* ");
+            System.out.println();
+            System.out.println();
+        }
+  
+        
+        
+        
+    }
+
+}
