7ddefcc7ca50c4616aa81e33a835dedad8af7238
softmoca <105472863+softmoca@users.noreply.github.com>
2024-04-23 09:47:13 +0900

    Title: 좌우반전시킨 정사각형, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240423/좌우반전시킨 정사각형/left-right-inverted-square.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 1; i <= n; i++) {
+        
+           
+                for (int j = n; j >0; j--) {
+                    System.out.print(i*j);
+                    System.out.print(' ');
+           
+                }
+          
+            System.out.println();
+        }
+    }
+}
