7d16d9e361559b591b58841fb905f422a86470e6
softmoca <105472863+softmoca@users.noreply.github.com>
2024-05-14 17:11:42 +0900

    Title: 벽이 있는 충돌 실험, Time: 2087ms, Memory: 125MB, Status: Passed - CodeTree

--- 240514/벽이 있는 충돌 실험/collision-experiment-with-wall.java
+++ 240514/벽이 있는 충돌 실험/collision-experiment-with-wall.java
@@ -1,6 +1,15 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
+class Marble {
+    int x, y, z;
+    public Marble(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+
 public class Main {
     public static final int ASCII_NUM = 128;
     public static final int MAX_N = 50;
@@ -18,7 +27,7 @@ public class Main {
     public static int[] dx = new int[]{-1, 0, 0, 1};
     public static int[] dy = new int[]{0, 1, -1, 0};
     
-    public static ArrayList<int[]> marbles = new ArrayList<>();
+    public static ArrayList<Marble> marbles = new ArrayList<>();
     
     // 해당 위치가 격자 안에 들어와 있는지 확인합니다.
     public static boolean inRange(int x, int y) {
@@ -27,28 +36,29 @@ public class Main {
     
     // 해당 구슬이 1초 후에 어떤 위치에서 어떤 방향을 보고 있는지를 구해
     // 그 상태를 반환합니다.
-    public static int[] Move(int[] marble) {
-        int x = marble[0];
-        int y = marble[1];
-        int dir = marble[2];
+    public static Marble Move(Marble marble) {
+        // tuple의 경우 다음과 같이 원하는 변수에 값을 뽑아줄 수 있습니다.
+        int x = marble.x;
+        int y = marble.y;
+        int dir = marble.z;
     
         // 바로 앞에 벽이 있는지 판단합니다.
         int nx = x + dx[dir], ny = y + dy[dir];
         
         // Case 1 : 벽이 없는 경우에는 그대로 한 칸 전진합니다.
         if(inRange(nx, ny))
-            return new int[]{nx, ny, dir};
+            return new Marble(nx, ny, dir);
         // Case 2 : 벽이 있는 경우에는 방향을 반대로 틀어줍니다.
         // 처음에 dx, dy를 dir 기준 0, 3이 대칭 1, 2가 대칭이 되도록
         // 설정해놨기 때문에 간단하게 처리가 가능합니다.
         else
-            return new int[]{x, y, 3 - dir};
+            return new Marble(x, y, 3 - dir);
     }
     
     // 구슬을 전부 한 번씩 움직여 봅니다.
     public static void moveAll() {
-        for(int i = 0; i < marbles.size(); i++) {
-            int[] nextMarble = Move(marbles.get(i));
+        for(int i = 0; i < (int) marbles.size(); i++) {
+            Marble nextMarble = Move(marbles.get(i));
             marbles.set(i, nextMarble);
         }
     }
@@ -57,32 +67,32 @@ public class Main {
     // 이를 위해 자신의 현재 위치에 놓인 구슬의 개수가 
     // 자신을 포함하여 2개 이상인지 확인합니다.
     public static boolean duplicateMarbleExist(int targetIdx) {
-        int targetX = marbles.get(targetIdx)[0];
-        int targetY = marbles.get(targetIdx)[1];
+        int targetX = marbles.get(targetIdx).x;
+        int targetY = marbles.get(targetIdx).y;
     
         return marbleCnt[targetX][targetY] >= 2;
     }
     
     // 충돌이 일어나는 구슬은 전부 지워줍니다.
     public static void removeDuplicateMarbles() {
-        ArrayList<int[]> tempVector = new ArrayList<>();
+        ArrayList<Marble> tempVector = new ArrayList<>();
         
         // Step2-1 : 각 구슬의 위치에 count를 증가시킵니다.
-        for(int i = 0; i < marbles.size(); i++) {
-            int x = marbles.get(i)[0];
-            int y = marbles.get(i)[1];
+        for(int i = 0; i < (int) marbles.size(); i++) {
+            int x = marbles.get(i).x;
+            int y = marbles.get(i).y;
             marbleCnt[x][y]++;
         }
     
         // Step2-2 : 충돌이 일어나지 않는 구슬만 전부 기록합니다.
-        for(int i = 0; i < marbles.size(); i++)
+        for(int i = 0; i < (int) marbles.size(); i++)
             if(!duplicateMarbleExist(i))
                 tempVector.add(marbles.get(i));
     
         // Step2-3 : 나중을 위해 각 구슬의 위치에 적어놓은 count 수를 다시 초기화합니다.
-        for(int i = 0; i < marbles.size(); i++) {
-            int x = marbles.get(i)[0];
-            int y = marbles.get(i)[1];
+        for(int i = 0; i < (int) marbles.size(); i++) {
+            int x = marbles.get(i).x;
+            int y = marbles.get(i).y;
             marbleCnt[x][y]--;
         }
     
@@ -122,7 +132,7 @@ public class Main {
                 int x = sc.nextInt();
                 int y = sc.nextInt();
                 char d = sc.next().charAt(0);
-                marbles.add(new int[]{x, y, mapper[d]});
+                marbles.add(new Marble(x, y, mapper[d]));
             }
 
             // 2*n번 이후에는 충돌이 절대 일어날 수 없으므로
@@ -131,7 +141,7 @@ public class Main {
                 simulate();
             
             // 출력:
-            System.out.println(marbles.size());
+            System.out.println((int) marbles.size());
         }
     }
 }
