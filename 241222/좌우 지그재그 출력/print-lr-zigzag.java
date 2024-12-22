2b4466827c238fdcd0652c7b3fa925903cf870f9
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2024-12-22 15:21:12 +0900

    Title: 좌우 지그재그 출력, Time: 194ms, Memory: 11MB, Status: Passed - CodeTree

--- 241222/좌우 지그재그 출력/print-lr-zigzag.java
+++ 241222/좌우 지그재그 출력/print-lr-zigzag.java
@@ -12,29 +12,26 @@ int cnt=1;
 for(int i=0;i<n;i++ ){
 
     if( i%2==0){
-        for(int j=0;j<n j++){
+        for(int j=0;j<n; j++){
             System.out.print(cnt+" ");
                  cnt++;
         }
         cnt=cnt+n-1;
 
     }else{
-              for(int j=0;j<n j++){
+              for(int j=0;j<n; j++){
              
             System.out.print(cnt+" ");
             cnt--;
         }
-        cnt=cnt+n=1;
+        cnt=cnt+n+1;
 
 
 
     }
     System.out.println();
 
-
-
 }
-
-
     }
+
 }
