085fafc0463c8ef0c585fda9f2fb35a751cebace
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-03-07 15:02:32 +0900

    Title: 삽입 정렬 구현, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250307/삽입 정렬 구현/implement-insertion-sort.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // 삽입 정렬 구현
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+
+        // 정렬된 배열 출력
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
