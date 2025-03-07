eea7f0bd36c1212a290800563e271a7d4340e6a9
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-03-07 14:27:33 +0900

    Title: 거품 정렬 구현, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250307/거품 정렬 구현/implement-bubble-sort.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        
+        // Bubble Sort
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        
+        // Print sorted array
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
