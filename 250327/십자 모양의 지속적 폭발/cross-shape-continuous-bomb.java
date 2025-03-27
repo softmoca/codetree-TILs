c94bfd86a7dddbc55180ca2fdeb51056198db8fc
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-03-27 20:49:25 +0900

    Title: 십자 모양의 지속적 폭발, Time: 277ms, Memory: 16MB, Status: Passed - Codetree

--- 250327/십자 모양의 지속적 폭발/cross-shape-continuous-bomb.java
+++ 250327/십자 모양의 지속적 폭발/cross-shape-continuous-bomb.java
@@ -106,7 +106,7 @@ public class Main {
         }
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.print(arr[i][j]);
+                System.out.print(arr[i][j]+" ");
             }
             System.out.println();
         }
