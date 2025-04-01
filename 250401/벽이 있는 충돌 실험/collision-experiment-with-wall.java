aefefc675299cd80b532307d9ad12d56a84cce13
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-04-01 13:35:13 +0900

    Title: 벽이 있는 충돌 실험, Time: 1612ms, Memory: 83MB, Status: Passed - Codetree

--- 250401/벽이 있는 충돌 실험/collision-experiment-with-wall.java
+++ 250401/벽이 있는 충돌 실험/collision-experiment-with-wall.java
@@ -1,84 +1,20 @@
 import java.util.Scanner;
-// 아주 많은 시간 처리 2*n보다 더 많을꺼 같지만 그려보니 2n인듯
 
 public class Main {
-    static int t;
-    static int n;
-    static int m;
-    static int[][] dirArr;
-    static int[][] nextDirArr;
-    static int[][] removeArr;
-    static char[] mapperDir = new char[128];
-    static int[] dx = {-1, 0, 0, 1};
-    static int[] dy = {0, 1, -1, 0};
+    static final int[] dx = {-1, 0, 0, 1}; // U R L D
+    static final int[] dy = {0, 1, -1, 0};
+    static final int MAX_ASCII = 128;
 
+    static int t, n, m;
+    static int[][] dirArr, nextDirArr, removeArr;
+    static final char[] DIR_CHAR = {'U', 'R', 'L', 'D'};
+    static final int[] DIR_MAP = new int[MAX_ASCII];
 
-    static void moveNext(int i, int j) {
-        int nx = i + dx[dirArr[i][j]];
-        int ny = j + dy[dirArr[i][j]];
-
-        if (nx < 0 || nx >= n || ny < 0 || ny >= n) {// 방향 전환
-            nextDirArr[i][j] = 3 - dirArr[i][j];
-            removeArr[i][j]++;
-        } else {
-            nextDirArr[nx][ny] = dirArr[i][j];
-            removeArr[nx][ny]++;
-        }
-
-
-    }
-
-    static void simulate() {
-        int time = 2 * n;
-
-        while (time-- > 0) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    nextDirArr[i][j] = -1;
-                }
-            }
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    removeArr[i][j] = 0;
-                }
-            }
-
-            //이동
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (dirArr[i][j] >= 0) {
-                        moveNext(i, j);
-                    }
-                }
-            }
-            //디버깅
-//            for (int i = 0; i < n; i++) {
-//                for (int j = 0; j < n; j++) {
-//                    System.out.print(removeArr[i][j] + " ");
-//                }
-//                System.out.println();
-//            }
-//            System.out.println();
-
-            // 충돌 제거
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (removeArr[i][j] > 1) {
-                        nextDirArr[i][j] = -1;
-                        removeArr[i][j] = 0;
-                    }
-                }
-            }
-
-            //원래 배열로 복사
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    dirArr[i][j] = nextDirArr[i][j];
-                }
-            }
-
-
-        }
+    static {
+        DIR_MAP['U'] = 0;
+        DIR_MAP['R'] = 1;
+        DIR_MAP['L'] = 2;
+        DIR_MAP['D'] = 3;
     }
 
     public static void main(String[] args) {
@@ -88,42 +24,82 @@ public class Main {
         while (t-- > 0) {
             n = sc.nextInt();
             m = sc.nextInt();
+
             dirArr = new int[n][n];
             nextDirArr = new int[n][n];
             removeArr = new int[n][n];
 
-            mapperDir['U'] = 0;
-            mapperDir['R'] = 1;
-            mapperDir['L'] = 2;
-            mapperDir['D'] = 3;
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    dirArr[i][j] = -1;
-                }
-            }
+            initArray(dirArr, -1);
 
             for (int i = 0; i < m; i++) {
                 int x = sc.nextInt() - 1;
                 int y = sc.nextInt() - 1;
                 char d = sc.next().charAt(0);
-                // removeArr[x][y] = 1;
-                dirArr[x][y] = mapperDir[d];
+                dirArr[x][y] = DIR_MAP[d];
             }
 
             simulate();
-            int res = 0;
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (dirArr[i][j] >= 0) {
-                        res++;
-                    }
-                }
-            }
-            System.out.println(res);
 
-
+            System.out.println(countRemaining());
         }
+    }
 
+    static void simulate() {
+        int time = 2 * n;
 
+        while (time-- > 0) {
+            initArray(nextDirArr, -1);
+            initArray(removeArr, 0);
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    if (dirArr[i][j] >= 0)
+                        move(i, j);
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    if (removeArr[i][j] > 1)
+                        nextDirArr[i][j] = -1;
+
+            copyArray(nextDirArr, dirArr);
+        }
+    }
+
+    static void move(int x, int y) {
+        int dir = dirArr[x][y];
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
+
+        if (!inRange(nx, ny)) {
+            nextDirArr[x][y] = 3 - dir;
+            removeArr[x][y]++;
+        } else {
+            nextDirArr[nx][ny] = dir;
+            removeArr[nx][ny]++;
+        }
+    }
+
+    static boolean inRange(int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < n;
+    }
+
+    static void initArray(int[][] arr, int val) {
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                arr[i][j] = val;
+    }
+
+    static void copyArray(int[][] from, int[][] to) {
+        for (int i = 0; i < n; i++)
+            System.arraycopy(from[i], 0, to[i], 0, n);
+    }
+
+    static int countRemaining() {
+        int res = 0;
+        for (int[] row : dirArr)
+            for (int d : row)
+                if (d >= 0)
+                    res++;
+        return res;
     }
 }
