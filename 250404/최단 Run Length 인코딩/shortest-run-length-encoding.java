38e5b779fa1cee4c41dad5831707e6761047eb3c
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-04-04 22:25:51 +0900

    Title: 최단 Run Length 인코딩, Time: 104ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250404/최단 Run Length 인코딩/shortest-run-length-encoding.java
@@ -0,0 +1,74 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class Main {
+
+    static int count(char[] chars) {
+        int cnt = 1;
+        int len = chars.length;
+
+        for (int i = 0; i < len - 1; i++) {
+            if (chars[i] != chars[i + 1]) {
+                cnt++;
+            }
+
+        }
+
+        if (cnt == 1 && len == 10) {
+            return 3;
+        }
+
+        return cnt * 2;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        int len = str.length();
+        int lenn = len;
+        int res = 10000;
+        char[] chars = str.toCharArray();
+
+        while (len-- >= 0) {
+
+            // 쉬프트
+
+            ArrayDeque<Character> ad = new ArrayDeque<>();
+            for (int i = 1; i < lenn; i++) {
+                ad.addLast(chars[i]);
+            }
+            ad.addLast(chars[0]);
+
+            for (int i = 0; i < lenn; i++) {
+                chars[i] = ad.removeFirst();
+            }
+
+            // run legnth길이 구하기
+            res = Math.min(res, count(chars));
+
+
+        }
+        System.out.println(res);
+
+    }
+
+    static int n;
+
+    static void initArray(Scanner sc, int[][] arr) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+    }
+
+    static void printArray(int[][] arr) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
