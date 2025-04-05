356c2d67380de341bedc24ee24ece180e2d1695c
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-04-05 13:13:39 +0900

    Title: 합쳐지는 구슬들, Time: 143ms, Memory: 10MB, Status: Passed - Codetree

--- 250405/합쳐지는 구슬들/merge-marbles.java
+++ 250405/합쳐지는 구슬들/merge-marbles.java
@@ -1,175 +1,184 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
-class Tuple {
-    int x, y, z;
+class Pair {
+    int idx, row, col, weight, dir;
 
-    public Tuple(int x, int y, int z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
+    Pair(int idx, int row, int col, int weight, int dir) {
+        this.idx = idx;
+        this.row = row;
+        this.col = col;
+        this.weight = weight;
+        this.dir = dir;
     }
+
+
 }
 
+
 public class Main {
-    public static final Tuple EMPTY = new Tuple(0, 0, 0);
-    public static final int DIR_NUM = 4;
-    public static final int ASCII_NUM = 128;
-    public static final int MAX_N = 50;
+    static int n;
+    static int m;
+    static int t;
+    static char[] dirMapper = new char[128];
+    static int[] dx = {-1, 0, 0, 1};
+    static int[] dy = {0, 1, -1, 0};
+    static ArrayList<Pair> pairs = new ArrayList<>();
+    static int[][] countArr;
+    static boolean[][] countArrC;
 
-    public static int n, m, t;
-    public static Tuple[][] grid = new Tuple[MAX_N][MAX_N];
-    public static Tuple[][] nextGrid = new Tuple[MAX_N][MAX_N];
+    static void moveAll() {
+        for (int i = 0; i < pairs.size(); i++) {
+            Pair currPair = pairs.get(i);
+            int nx = currPair.row + dx[currPair.dir];
+            int ny = currPair.col + dy[currPair.dir];
 
-    public static boolean inRange(int x, int y) {
-        return 0 <= x && x < n && 0 <= y && y < n;
-    }
-
-    public static Tuple NextPos(int x, int y, int moveDir) {
-        int[] dx = new int[]{-1, 0, 0, 1};
-        int[] dy = new int[]{0, 1, -1, 0};
-
-        int nx = x + dx[moveDir], ny = y + dy[moveDir];
-        if (!inRange(nx, ny)) {
-            moveDir = 3 - moveDir;
-        } else {
-            x = nx;
-            y = ny;
-        }
-
-        return new Tuple(x, y, moveDir);
-    }
-
-    // (x, y) 위치에 새로운 구슬이 들어왔을 때 갱신을 진행합니다.
-    public static void update(int x, int y, Tuple newMarble) {
-        // 기존 구슬 정보입니다.
-        int num = nextGrid[x][y].x;
-        int weight = nextGrid[x][y].y;
-        int moveDir = nextGrid[x][y].z;
-
-        // 새롭게 들어온 구슬 정보입니다.
-        int newNum = newMarble.x;
-        int newWeight = newMarble.y;
-        int newDir = newMarble.z;
-
-        // 새로 들어온 구슬이 더 우선순위가 높다면
-        // 번호와 방향은 새로운 구슬을 따르게 됩니다.
-        if (newNum > num) {
-            nextGrid[x][y] = new Tuple(newNum, weight + newWeight, newDir);
-        }
-        // 기존 구슬의 우선순위가 더 높다면
-        // 무게만 더해집니다.
-        else {
-            nextGrid[x][y] = new Tuple(num, weight + newWeight, moveDir);
-        }
-    }
-
-    public static void move(int x, int y) {
-        int num = grid[x][y].x;
-        int weight = grid[x][y].y;
-        int moveDir = grid[x][y].z;
-
-        // Step1. 현재 구슬의 다음 위치와 방향을 구합니다.
-        Tuple nextPos = NextPos(x, y, moveDir);
-        int nx = nextPos.x;
-        int ny = nextPos.y;
-        int nextDir = nextPos.z;
-
-        // Step2. 구슬을 옮겨줍니다.
-        update(nx, ny, new Tuple(num, weight, nextDir));
-    }
-
-    public static void simulate() {
-        // Step1. nextGrid를 초기화합니다.
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                nextGrid[i][j] = EMPTY;
+            if (nx < 0 || nx >= n || ny < 0 || ny >= n) {
+                currPair.dir = 3 - currPair.dir;
+            } else {
+                currPair.row = nx;
+                currPair.col = ny;
             }
+
+
         }
 
-        // Step2. 각 구슬들을 한 칸씩 움직여줍니다.
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j].x != EMPTY.x || grid[i][j].y != EMPTY.y || grid[i][j].z != EMPTY.z) {
-                    move(i, j);
+    }
+
+    static void boom() {
+        // Step 1: countArr 초기화
+        for (Pair p : pairs) {
+            countArr[p.row][p.col]++;
+        }
+
+        // Step 2: 충돌이 일어난 위치 별로 최종 Pair 저장
+        Map<String, Pair> mergedMarbles = new HashMap<>();
+
+        for (Pair p : pairs) {
+            int x = p.row;
+            int y = p.col;
+
+            if (countArr[x][y] >= 2) { // 충돌하는 경우
+                String key = x + "," + y;
+
+                if (!mergedMarbles.containsKey(key)) {
+                    mergedMarbles.put(key, new Pair(p.idx, x, y, p.weight, p.dir));
+                } else {
+                    Pair existing = mergedMarbles.get(key);
+                    int newWeight = existing.weight + p.weight;
+
+                    // 번호가 더 크면 dir, idx 업데이트
+                    if (existing.idx < p.idx) {
+                        mergedMarbles.put(key, new Pair(p.idx, x, y, newWeight, p.dir));
+                    } else {
+                        mergedMarbles.put(key, new Pair(existing.idx, x, y, newWeight, existing.dir));
+                    }
                 }
             }
         }
 
-        // Step3. nextGrid 값을 grid로 옮겨줍니다.
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                grid[i][j] = nextGrid[i][j];
+        // Step 3: 충돌 없는 구슬 + 충돌 후 살아남은 구슬로 리스트 갱신
+        ArrayList<Pair> temp = new ArrayList<>();
+
+        for (Pair p : pairs) {
+            int x = p.row;
+            int y = p.col;
+            String key = x + "," + y;
+
+            if (countArr[x][y] >= 2) {
+                if (mergedMarbles.containsKey(key)) {
+                    temp.add(mergedMarbles.get(key));
+                    mergedMarbles.remove(key); // 이미 추가된 위치는 다시 추가하지 않음
+                }
+            } else {
+                temp.add(p);
             }
         }
+
+        // Step 4: countArr 초기화
+        for (Pair p : pairs) {
+            countArr[p.row][p.col] = 0;
+        }
+
+        pairs = temp;
     }
 
-    public static int getMarbleNum() {
-        int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] != EMPTY) {
-                    cnt++;
-                }
-            }
-        }
+    static void simulate() {
 
-        return cnt;
-    }
+        moveAll();
+        // 디버깅
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < n; j++) {
+//                System.out.print(countArr[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+//
+//        for (int o = 0; o < pairs.size(); o++) {
+//            Pair curr = pairs.get(o);
+//            System.out.println(curr.idx + " " + curr.row + " " + curr.col
+//                    + " " + curr.weight + " " + curr.dir);
+//        }
+//        System.out.println();
 
-    public static int getMaxWeight() {
-        int maxWeight = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] != EMPTY) {
-                    int weight = grid[i][j].y;
-                    maxWeight = Math.max(maxWeight, weight);
-                }
-            }
-        }
+        boom();
 
-        return maxWeight;
+
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+
+        /*
+        1. 모든 구슬 움직이기 -> 범위 벗어나면 방향 반대
+        2. 충돌확인
+        2-1. 충돌시 -> 무게는 합쳐진다. 방향은 큰 번호, 번호도 큰 번호
+         */
         n = sc.nextInt();
         m = sc.nextInt();
         t = sc.nextInt();
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                grid[i][j] = new Tuple(0, 0, 0);
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                nextGrid[i][j] = new Tuple(0, 0, 0);
-            }
-        }
-
-        int[] dirMapper = new int[ASCII_NUM];
+        countArr = new int[n][n];
+        countArrC = new boolean[n][n];
         dirMapper['U'] = 0;
         dirMapper['R'] = 1;
         dirMapper['L'] = 2;
         dirMapper['D'] = 3;
 
         for (int i = 0; i < m; i++) {
-            int r = sc.nextInt();
-            int c = sc.nextInt();
+            int row = sc.nextInt() - 1;
+            int col = sc.nextInt() - 1;
             char d = sc.next().charAt(0);
             int w = sc.nextInt();
-            grid[r - 1][c - 1] = new Tuple(i + 1, w, dirMapper[d]);
+            int dir = dirMapper[d];
+            pairs.add(new Pair(i, row, col, w, dir));
         }
 
-        // t초에 걸쳐 시뮬레이션을 진행합니다.
         while (t-- > 0) {
             simulate();
         }
 
-        int marbleNum = getMarbleNum();
-        int maxWeight = getMaxWeight();
+        System.out.print(pairs.size() + " ");
+        int res = 0;
+        for (int i = 0; i < pairs.size(); i++) {
+            res = Math.max(res, pairs.get(i).weight);
 
-        System.out.print(marbleNum + " " + maxWeight);
+        }
+        System.out.print(res);
+
+
     }
 }
+
+
+/*
+
+4 5 2
+1 2 L 5
+2 3 U 2
+3 1 R 2
+4 2 U 3
+3 4 D 5
+ */
