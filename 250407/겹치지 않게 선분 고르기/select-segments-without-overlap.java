27d46d591ac2aeaae315660473a901e2c66f6beb
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-04-07 16:50:10 +0900

    Title: 겹치지 않게 선분 고르기, Time: 163ms, Memory: 10MB, Status: Passed - Codetree

--- 250407/겹치지 않게 선분 고르기/select-segments-without-overlap.java
+++ 250407/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -1,12 +1,6 @@
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
-//       for (int i = 0; i < anwer.size(); i++) {
-////                System.out.print(anwer.get(i) + " ");
-////            }
-////            System.out.println();
 public class Main {
 
     static class Line {
@@ -16,29 +10,24 @@ public class Main {
             this.x = x;
             this.y = y;
         }
-
     }
 
     static int n;
     static Line[] lines;
-    static List<Integer> anwer = new ArrayList<>();
     static int[] tempLine = new int[1001];
     static int res = 0;
 
-    static void init() {
-        for (int i = 0; i < 1001; i++) {
-            tempLine[i] = 0;
-        }
+    static boolean[] cc;
+    static boolean[] vi;
 
-    }
-
-    static boolean check() {
-        for (int i = 0; i < anwer.size(); i++) {
-            int startX = lines[anwer.get(i)].x;
-            int startY = lines[anwer.get(i)].y;
-
-            for (int j = startX; j <= startY; j++) {
-                tempLine[j]++;
+    static boolean dfsCheck() {
+        for (int i = 0; i < n; i++) {
+            if (cc[i]) {
+                int startX = lines[i].x;
+                int startY = lines[i].y;
+                for (int j = startX; j <= startY; j++) {
+                    tempLine[j]++;
+                }
             }
         }
 
@@ -51,43 +40,59 @@ public class Main {
         return true;
     }
 
-    static void c(int currIdx, int cnt, int s) {
-        if (currIdx == cnt + 1) {
+    static void dfs(int idx) {
+        if (idx == n) {
+            for (int i = 0; i < 1001; i++) {
+                tempLine[i] = 0;
+            }
+            if (dfsCheck()) {
+                int cnt = 0;
 
-            if (check()) {
-
-//                System.out.println(cnt);
-//                System.exit(0);
+                for (int i = 0; i < n; i++) {
+                    if (cc[i]) {
+                        cnt++;
+                    }
+                }
                 res = Math.max(res, cnt);
 
-            }
-            init();
 
+            }
             return;
         }
 
-        for (int i = s; i < n; i++) {
-            anwer.add(i);
-            c(currIdx + 1, cnt, i + 1);
-            anwer.remove(anwer.size() - 1);
+        if (!vi[idx]) {
+            vi[idx] = true;
+            cc[idx] = true;
+            dfs(idx + 1);
+            vi[idx] = false;
+
+
         }
 
+        if (!vi[idx]) {
+            cc[idx] = false;
+            dfs(idx + 1);
+            vi[idx] = false;
+        }
+
+
     }
+
 
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         lines = new Line[n];
-
+        vi = new boolean[n];
+        cc = new boolean[n];
         for (int i = 0; i < n; i++) {
             int a = sc.nextInt();
             int b = sc.nextInt();
             lines[i] = new Line(a, b);
         }
 
-        for (int cnt = n; cnt > 0; cnt--) {
-            c(1, cnt, 0);
-        }
+        dfs(0);
+
         System.out.println(res);
 
     }
