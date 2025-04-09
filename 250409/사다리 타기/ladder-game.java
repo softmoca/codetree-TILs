e19a4368d5621dfaa809d8b44cb10e136867e12e
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-04-09 12:46:57 +0900

    Title: 사다리 타기, Time: 189ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 250409/사다리 타기/ladder-game.java
@@ -0,0 +1,141 @@
+import java.io.*;
+import java.util.*;
+
+
+
+public class Main {
+    static class Pair {
+        int col, row;
+
+        Pair(int col, int row) {
+            this.col = col;
+            this.row = row;
+        }
+
+    }
+
+    static int n;
+    static int maxM;
+    static int m;
+    static Pair[] pairs;
+    static int[][] arrTemp;
+    static int[] ansTemp;
+    static List<Pair> answer = new ArrayList<>();
+    static int res = Integer.MAX_VALUE;
+    static int[] ans;
+
+    static void init() {
+        for (int i = 0; i < maxM + 2; i++) {
+            for (int j = 0; j < n * 2; j++) {
+                arrTemp[i][j] = 0;
+            }
+        }
+
+        for (int i = 1; i < 2 * n; i += 2) {
+            arrTemp[0][i] = i;
+        }
+
+        for (int i = 0; i < 2 * n; i++) {
+            ansTemp[i] = 0;
+        }
+    }
+
+    static void draw() {
+        for (int i = 0; i < answer.size(); i++) {
+            int col = answer.get(i).col;
+            int row = answer.get(i).row;
+            col = col * 2 - 1;
+            arrTemp[row][col + 1] = -1;
+        }
+    }
+    static void makeAnsTemp() {
+        for (int col = 1; col < 2 * n; col += 2) {
+            int tempCol = col;
+            for (int row = 0; row < maxM; row++) {
+                if (tempCol - 1 != 0 && arrTemp[row + 1][tempCol - 1] == -1) {
+                    tempCol -= 2;
+                } else if (tempCol + 1 != 2 * n && arrTemp[row + 1][tempCol + 1] == -1) {
+                    tempCol += 2;
+                }
+            }
+            ansTemp[tempCol] = col;
+        }
+
+    }
+
+    static boolean ok() {
+        for (int i = 0; i < 2 * n; i++) {
+            if (ans[i] != ansTemp[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    static void choose(int currIdx) {
+        if (currIdx == m) {
+
+            init();
+            draw();
+            makeAnsTemp();
+            if (ok())  res = Math.min(res, answer.size());
+            return;
+        }
+
+        answer.add(pairs[currIdx]);
+        choose(currIdx + 1);
+        answer.remove(answer.size() - 1);
+        choose(currIdx + 1);
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        pairs = new Pair[m];
+        maxM = m;
+
+        for (int i = 0; i < m; i++) {
+            int col = sc.nextInt();
+            int row = sc.nextInt();
+            maxM = Math.max(maxM, row);
+
+            pairs[i] = new Pair(col, row);
+        }
+
+        int[][] arr = new int[maxM + 2][n * 2];
+        arrTemp = new int[maxM + 2][n * 2];
+
+        for (int i = 1; i < 2 * n; i += 2) {
+            arr[0][i] = i;
+        }
+
+        for (int i = 0; i < m; i++) {
+            arr[pairs[i].row][pairs[i].col * 2] = -1;
+        }
+
+        ans = new int[2 * n];
+        ansTemp = new int[2 * n];
+
+        for (int col = 1; col < 2 * n; col += 2) {
+            int tempCol = col;
+            for (int row = 0; row < maxM; row++) {
+                if (tempCol - 1 != 0 && arr[row + 1][tempCol - 1] == -1) {
+                    tempCol -= 2;
+                } else if (tempCol + 1 != 2 * n && arr[row + 1][tempCol + 1] == -1) {
+                    tempCol += 2;
+                }
+            }
+            ans[tempCol] = col;
+        }
+
+        // 조합 시작
+
+        choose(0);
+        System.out.println(res);
+
+    }
+}
