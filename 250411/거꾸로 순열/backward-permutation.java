5a5596591258a473b7fa818f824609d387d6f810
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-04-11 13:12:29 +0900

    Title: 거꾸로 순열, Time: 1787ms, Memory: 51MB, Status: Passed - Codetree

---
+++ 250411/거꾸로 순열/backward-permutation.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n;
+    static List<Integer> answer = new ArrayList<>();
+    static boolean[] visited;
+
+    static void choose(int currIdx) {
+        if (currIdx == n) {
+            for (int i = 0; i < answer.size(); i++) {
+                System.out.print(answer.get(i) + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+
+        for (int i = n; i >= 1; i--) {
+            if (visited[i]) continue;
+
+            answer.add(i);
+            visited[i] = true;
+            choose(currIdx + 1);
+            answer.remove(answer.size() - 1);
+            visited[i] = false;
+
+
+        }
+
+
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        visited = new boolean[n + 1];
+        choose(0);
+
+
+    }
+}
