f9425ebe0b4bee44e74e16ec1c615a56e6137feb
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-04-11 15:21:28 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 230ms, Memory: 21MB, Status: Passed - Codetree

--- 250411/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
+++ 250411/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
@@ -1,55 +1,54 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    static int n;
-    static int m;
-    static int[][] board;
-    static int[] dx = {1, 0};
-    static int[] dy = {0, 1};
-    static boolean flag = false;
-    static boolean[][] visited;
+    public static final int MAX_NUM = 100;
+    public static final int DIR_NUM = 2;
 
-    static void dfs(int x, int y) {
+    public static int n, m;
+    public static int[][] grid = new int[MAX_NUM][MAX_NUM];
+    public static int[][] visited = new int[MAX_NUM][MAX_NUM];
 
-        if (x == n - 1 && y == m - 1) {
-            System.out.println(1);
-            System.exit(0);
-        }
-
-        for (int k = 0; k < 2; k++) {
-            int nx = x + dx[k];
-            int ny = y + dy[k];
-
-            if (nx < n && ny < m && board[nx][ny] == 1) {
-        
-                dfs(nx, ny);
-             
-            }
-
-
-        }
-        
-
+    // 탐색하는 위치가 격자 범위 내에 있는지 여부를 반환합니다.
+    public static boolean inRange(int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < m;
     }
 
-    public static void main(String[] args) throws IOException {
+    // 탐색하는 위치로 움직일 수 있는지 여부를 반환합니다.
+    public static boolean canGo(int x, int y) {
+        if(!inRange(x, y))
+            return false;
+        if(visited[x][y] == 1 || grid[x][y] == 0)
+            return false;
+        return true;
+    }
+
+    public static void DFS(int x, int y) {
+        int[] dx = new int[]{0, 1};
+        int[] dy = new int[]{1, 0};
+
+        // 아래쪽과 오른쪽 각각에 대하여 DFS 탐색을 합니다.
+        for(int i = 0; i < DIR_NUM; i++) {
+            int newX = x + dx[i];
+            int newY = y + dy[i];
+            if(canGo(newX, newY)) {
+                visited[newX][newY] = 1;
+                DFS(newX, newY);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         m = sc.nextInt();
-        board = new int[n][m];
-        visited = new boolean[n][m];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                board[i][j] = sc.nextInt();
-            }
-        }
-        visited[0][0] = true;
-        dfs(0, 0);
-        System.out.println(0);
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+                grid[i][j] = sc.nextInt();
 
+        visited[0][0] = 1;
+        DFS(0, 0);
 
+        System.out.println(visited[n - 1][m - 1]);
     }
 }
-
