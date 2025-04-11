52bd0161cf3e87942b5d67e15ebc0c59baf275c1
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-04-11 14:54:23 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 3045ms, Memory: 21MB, Status: Time Limit Exceed - Codetree

---
+++ 250411/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int m;
+    static int[][] board;
+    static int[] dx = {0, 1};
+    static int[] dy = {1, 0};
+    static boolean flag = false;
+
+    static boolean dfs(int x, int y) {
+
+        if (x == n - 1 && y == m - 1) flag = true;
+
+        for (int k = 0; k < 2; k++) {
+            int nx = x + dx[k];
+            int ny = y + dy[k];
+
+            if (nx >= 0 && ny >= 0 && nx < n && ny < m && board[nx][ny] == 1) {
+                dfs(nx, ny);
+            }
+
+
+        }
+        return false;
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        board = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                board[i][j] = sc.nextInt();
+            }
+        }
+
+        dfs(0, 0);
+
+        if (flag) {
+            System.out.println(1);
+        } else
+            System.out.println(0);
+
+    }
+}
