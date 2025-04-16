ba42f23c84c379a51011f713d269951bb5dd7fe8
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-04-16 10:31:01 +0900

    Title: N개 중에 M개 뽑기, Time: 147ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250416/N개 중에 M개 뽑기/n-choose-m.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n;
+    static int m;
+    static List<Integer> answer = new ArrayList<>();
+
+    static void choose(int currIdx, int startIdx) {
+        if (currIdx == m) {
+            for (int i = 0; i < answer.size(); i++) {
+                System.out.print(answer.get(i) + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = startIdx; i <= n; i++) {
+            answer.add(i);
+            choose(currIdx + 1, i + 1);
+            answer.remove(answer.size() - 1);
+        }
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        choose(0, 1);
+
+    }
+}
