172cc2a517967d49697bad74bdf90777a0dc3bd4
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-04-16 18:06:30 +0900

    Title: 피보나치 수, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

--- 250416/피보나치 수/fibonacci-number.java
+++ 250416/피보나치 수/fibonacci-number.java
@@ -2,29 +2,21 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] dp = new int[46];
 
-    static int fibo(int x) {
-
-        if (x == 1 || x == 2) {
-            return 1;
-        }
-
-        if (dp[x] == 0) {
-            return dp[x] = fibo(x - 1) + fibo(x - 2);
-        } else {
-            return dp[x];
-        }
-
-
-    }
 
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
 
+        int[] dp = new int[46];
         int n = sc.nextInt();
 
-        System.out.println(fibo(n));
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for (int i = 3; i < n + 1; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        System.out.println(dp[n]);
 
 
     }
