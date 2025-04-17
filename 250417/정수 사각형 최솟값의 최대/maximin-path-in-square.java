84f2b7ff1f8132f0a094c73e245e0d0eec5a765f
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-04-17 14:34:34 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 290ms, Memory: 31MB, Status: Passed - Codetree

--- 250417/정수 사각형 최솟값의 최대/maximin-path-in-square.java
+++ 250417/정수 사각형 최솟값의 최대/maximin-path-in-square.java
@@ -1,37 +1,43 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
+    public static final int MAX_NUM = 1000;
+    
+    public static int n;
+    public static int[][] num = new int[MAX_NUM][MAX_NUM];
+    public static int[][] dp = new int[MAX_NUM][MAX_NUM];
+    
+    public static void initialize() {
+        // 시작점의 경우 dp[0][0] = num[0][0]으로 초기값을 설정해줍니다
+        dp[0][0] = num[0][0];
+    
+        // 최좌측 열의 초기값을 설정해줍니다.
+        for(int i = 1; i < n; i++)
+            dp[i][0] = Math.min(dp[i-1][0], num[i][0]);
+    
+        // 최상단 행의 초기값을 설정해줍니다.
+        for(int j = 1; j < n; j++)
+            dp[0][j] = Math.min(dp[0][j-1], num[0][j]);
+    }
 
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
 
-        int n = sc.nextInt();
-        int[][] arr = new int[n][n];
-        int[][] dp = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = sc.nextInt();
-            }
-        }
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                num[i][j] = sc.nextInt();
 
-        dp[0][0] = arr[0][0];
+        // 초기값 설정
+        initialize();
 
-        for (int i = 1; i < n; i++) {
-            dp[0][i] = Math.min(dp[0][i - 1], arr[0][i]);
-            dp[i][0] = Math.min(dp[i - 1][0], arr[i][0]);
-        }
-
-   for (int i = 1; i < n; i++) {
-            for (int j = 1; j < n; j++) {
-                int fromTop = Math.min(dp[i - 1][j], arr[i][j]);
-                int fromLeft = Math.min(dp[i][j - 1], arr[i][j]);
-                dp[i][j] = Math.max(fromTop, fromLeft);
-            }
-        }
-        System.out.println(dp[n - 1][n - 1]);
+        // 탐색하는 위치의 위에 값과 좌측 값 중에 큰 값과
+        // 해당 위치의 숫자 중에 최솟값을 구해줍니다.
+        for(int i = 1; i < n; i++)
+            for(int j = 1; j < n; j++)
+                dp[i][j] = Math.min(Math.max(dp[i-1][j], dp[i][j-1]), num[i][j]);
 
 
+        System.out.print(dp[n-1][n-1]);
     }
 }
