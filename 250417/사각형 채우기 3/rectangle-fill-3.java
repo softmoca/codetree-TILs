507fc78adc204823cd4b7f2e03fd49e79f4c593a
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-04-17 12:02:18 +0900

    Title: 사각형 채우기 3, Time: 136ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250417/사각형 채우기 3/rectangle-fill-3.java
+++ 250417/사각형 채우기 3/rectangle-fill-3.java
@@ -16,7 +16,16 @@ public class Main {
         dp[5] = 226;
         long sum;
         for (int i = 6; i < n + 1; i++) {
-            sum = (dp[i - 1] * 2 + dp[i - 2] * 3 + dp[i - 3] * 2 + 2) % 1000000007;
+            if(i%2==0){
+                sum = (dp[i - 1] * 2 + dp[i - 2] * 3 + dp[i - 3] * 2 + 2) % 1000000007;
+                
+            }else{
+                sum = (dp[i - 1] * 2 + dp[i - 2] * 3 + dp[i - 3] * 2 ) % 1000000007;
+            }
+            
+            
+            
+            
             for (int j = 4; i - j > 0; j += 2) {
                 sum = (sum + dp[i - j] * 2) % 1000000007;
             }
