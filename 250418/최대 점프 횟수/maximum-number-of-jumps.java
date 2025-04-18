543b8ecdb9b7007248c86567361b282aa0933b95
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-04-18 13:10:59 +0900

    Title: 최대 점프 횟수, Time: 131ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250418/최대 점프 횟수/maximum-number-of-jumps.java
+++ 250418/최대 점프 횟수/maximum-number-of-jumps.java
@@ -18,12 +18,25 @@ public class Main {
         dp[0] = 0;
 
         for (int i = 0; i < n; i++) {
+
+           
+
+
             int cnt = arr[i];
+            if (dp[i] == 0 && arr[i] == 0) break;
+
             for (int j = i + 1; j < i + cnt + 1; j++) {
                 if (j >= n) break;
-                
+
                 dp[j] = Math.max(dp[j], dp[i] + 1);
             }
+
+            for (int j = 0; j < n; j++) {
+                System.out.print(dp[j] + " ");
+            }
+            System.out.println();
+
+
         }
         int res = 0;
         for (int i = 0; i < n; i++) {
