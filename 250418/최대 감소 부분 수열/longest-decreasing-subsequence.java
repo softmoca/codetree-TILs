a2f7a79c4fc85ae7b03eaee81892f02bdacfaad8
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-04-18 12:00:12 +0900

    Title: 최대 감소 부분 수열, Time: 215ms, Memory: 12MB, Status: Passed - Codetree

--- 250418/최대 감소 부분 수열/longest-decreasing-subsequence.java
+++ 250418/최대 감소 부분 수열/longest-decreasing-subsequence.java
@@ -15,18 +15,21 @@ public class Main {
         }
 
         dp[0] = 1;
+        for (int i = 0; i < n; i++) {
+            dp[i]=1;
+        }
 
 
         for (int i = 1; i < n; i++) {
             for (int j = i - 1; j >= 0; j--) {
-                if(arr[i]<arr[j]){
-                    dp[i]=Math.max(dp[i],dp[j]+1);
-                    
+                if (arr[i] < arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+
                 }
-                
+
             }
         }
-        
+
         int res = 0;
         for (int i = 0; i < n; i++) {
             res = Math.max(res, dp[i]);
