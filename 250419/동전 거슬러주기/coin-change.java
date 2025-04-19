976acdcb205d860658bdd2d33a483e6aee41ca55
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-04-19 13:06:17 +0900

    Title: 동전 거슬러주기, Time: 132ms, Memory: 10MB, Status: Passed - Codetree

--- 250419/동전 거슬러주기/coin-change.java
+++ 250419/동전 거슬러주기/coin-change.java
@@ -33,55 +33,13 @@ public class Main {
 //            System.out.println();
 
         }
-import java.io.*;
-import java.util.*;
-
-        public class Main {
-
-
-            public static void main(String[] args) throws IOException {
-                Scanner sc = new Scanner(System.in);
-                int n = sc.nextInt();
-                int m = sc.nextInt();
-                int[] coins = new int[n];
-                int[] dp = new int[m + 1];
-                for (int i = 0; i < n; i++) {
-                    coins[i] = sc.nextInt();
-                }
-
-                for (int i = 1; i <= m; i++) {
-                    dp[i] = Integer.MAX_VALUE;
-                }
-
-
-                for (int i = 1; i <= m; i++) {
-                    for (int j = 0; j < n; j++) {
-                        if (i >= coins[j]) {
-                            if (dp[i - coins[j]] == Integer.MAX_VALUE) continue;
-
-                            dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
-                        }
-                    }
-//            for (int j = 0; j < m + 1; j++) {
-//                System.out.print(dp[j] + " ");
-//            }
-//            System.out.println();
-
-                }
-
-                System.out.println(dp[m]);
-
-
-            }
-        }
-
+        
         if(dp[m]==Integer.MAX_VALUE){
             System.out.println(-1);
         }else{
             System.out.println(dp[m]);
-
+            
         }
-
 
 
     }
