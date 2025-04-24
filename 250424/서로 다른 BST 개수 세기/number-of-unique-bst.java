cf618d8bcaad76b58d6a3a6e4e6206ab1009d88d
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-04-24 13:09:20 +0900

    Title: 서로 다른 BST 개수 세기, Time: 114ms, Memory: 10MB, Status: Runtime Error - Codetree

--- 250424/서로 다른 BST 개수 세기/number-of-unique-bst.java
+++ 250424/서로 다른 BST 개수 세기/number-of-unique-bst.java
@@ -7,19 +7,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] dp=new int[n+1];
-        
-        dp[0]=1;
-        dp[1]=1;
-        dp[2]=2;
-        for (int i = 3; i < n+1; i++) {
-            for (int j = 0; j <n ; j++) {
-                dp[i]=dp[j]*dp[i-1-j];
+        int[] dp = new int[n + 1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+        //dp[2] = 2;
+        for (int i = 2; i < n + 1; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i] = dp[i] + dp[j] * dp[i - 1 - j];
             }
         }
         System.out.println(dp[n]);
-        
-        
-        
+
+
     }
 }
