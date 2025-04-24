26c5d296bc467689891576434076da8e88447866
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-04-24 13:13:55 +0900

    Title: 서로 다른 BST 개수 세기, Time: 135ms, Memory: 10MB, Status: Passed - Codetree

--- 250424/서로 다른 BST 개수 세기/number-of-unique-bst.java
+++ 250424/서로 다른 BST 개수 세기/number-of-unique-bst.java
@@ -13,7 +13,7 @@ public class Main {
         dp[1] = 1;
         //dp[2] = 2;
         for (int i = 2; i < n + 1; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < i; j++) {
                 dp[i] = dp[i] + dp[j] * dp[i - 1 - j];
             }
         }
