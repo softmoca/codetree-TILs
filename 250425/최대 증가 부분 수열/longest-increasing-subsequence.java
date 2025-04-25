640f53f4eedfb887d878d75e62f833a129dad872
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-04-25 16:21:34 +0900

    Title: 최대 증가 부분 수열, Time: 212ms, Memory: 12MB, Status: Passed - Codetree

---
+++ 250425/최대 증가 부분 수열/longest-increasing-subsequence.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int[] dp = new int[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            dp[i] = 1;
+        }
+
+        for (int i = 2; i < n + 1; i++) {
+
+            for (int j = 1; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+
+
+        }
+        int res = 0;
+        for (int i = 0; i < n + 1; i++) {
+            res = Math.max(dp[i], res);
+        }
+
+        System.out.println(res);
+    }
+}
