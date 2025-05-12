1ce14a2dea40bc1cc8f45e2034d57ce51c7825f3
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-05-12 18:05:55 +0900

    Title: 두 수의 합, Time: 843ms, Memory: 35MB, Status: Passed - Codetree

--- 250512/두 수의 합/sum-of-two-num.java
+++ 250512/두 수의 합/sum-of-two-num.java
@@ -1,34 +1,38 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
+import java.util.HashMap;
 
 public class Main {
+    public static final int MAX_N = 100000;
 
+    public static int n, k;
+    public static long[] arr = new long[MAX_N];
+    public static HashMap<Long, Integer> count = new HashMap<>();
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-
-        int[] arr = new int[n];
-        Map<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < n; i++) {
+        n = sc.nextInt();
+        k = sc.nextInt();
+        for (int i = 0; i < n; i++)
             arr[i] = sc.nextInt();
-            map.put(arr[i], k - arr[i]);
 
-        }
-
-        int res = 0;
+        // 1단계: 숫자별 등장 횟수를 모두 저장
         for (int i = 0; i < n; i++) {
-            if (map.containsKey(map.get(arr[i]))) {
-                res++;
-            }
-
-
+            count.put(arr[i], count.getOrDefault(arr[i], 0) + 1);
         }
-        System.out.println((res + 1) / 2);
 
+        int ans = 0;
 
+        // 2단계: 가능한 쌍을 계산
+        for (int i = 0; i < n; i++) {
+            long diff = k - arr[i];
+
+            // 현재 arr[i]를 하나 소모했으므로 미리 count 감소
+            count.put(arr[i], count.get(arr[i]) - 1);
+
+            if (count.containsKey(diff))
+                ans += count.get(diff);
+        }
+
+        System.out.print(ans);
     }
 }
