b4b7b86cd6a00ad77c4e8bfe83c2214f2a277cbf
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-05-12 11:32:46 +0900

    Title: 서로 다른 BST 개수 세기, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250512/서로 다른 BST 개수 세기/number-of-unique-bst.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] dp = new int[n + 1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+        //dp[2] = 2;
+        for (int i = 2; i < n + 1; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] = dp[i] + dp[j] * dp[i - 1 - j];
+            }
+        }
+        System.out.println(dp[n]);
+
+
+    }
+}
