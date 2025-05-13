11b6cdce473d51d139748d30a1ecd9253ed565e2
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-05-13 20:50:19 +0900

    Title: 자주 등장한 top K 숫자, Time: 1558ms, Memory: 38MB, Status: Passed - Codetree

---
+++ 250513/자주 등장한 top K 숫자/top-k-frequent-elements.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static class Pair {
+        int num, cnt;
+
+        public Pair(int num, int cnt) {
+            this.num = num;
+            this.cnt = cnt;
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+
+        Pair[] pairs = new Pair[map.size()];
+
+
+        int idx = 0;
+        for (int x : map.keySet()) {
+            pairs[idx++] = new Pair(x, map.get(x));
+
+        }
+
+        Arrays.sort(pairs,
+                Comparator.comparing((Pair p) -> -p.cnt).
+                        thenComparing((Pair p) -> -p.num)
+        );
+
+        for (int i = 0; i < k; i++) {
+            System.out.print(pairs[i].num + " ");
+
+        }
+
+
+    }
+}
