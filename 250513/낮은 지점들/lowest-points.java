3ec078fd932dd0d956f3d93928177d652ebe4783
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-05-13 21:35:13 +0900

    Title: 낮은 지점들, Time: 994ms, Memory: 36MB, Status: Wrong Answer - Codetree

---
+++ 250513/낮은 지점들/lowest-points.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            if (map.containsKey(x)) {
+                if (map.get(x) > y) {
+                    map.put(x, y);
+                }
+
+
+            } else {
+                map.put(x, y);
+            }
+
+        }
+
+        int res = 0;
+        for (int x : map.keySet()) {
+            res += map.get(x);
+        }
+        System.out.println(res);
+
+    }
+}
