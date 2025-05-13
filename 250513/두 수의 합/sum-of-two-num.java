82d0d15f18ba80d2e2815198b7604d33b1afdfc6
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-05-13 15:43:29 +0900

    Title: 두 수의 합, Time: 605ms, Memory: 36MB, Status: Passed - Codetree

--- 250513/두 수의 합/sum-of-two-num.java
+++ 250513/두 수의 합/sum-of-two-num.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 
 public class Main {
     public static final int MAX_N = 100000;
-    
+
     // 변수 선언
     public static int n, k;
     public static long[] arr = new long[MAX_N];
@@ -14,23 +14,24 @@ public class Main {
         // 입력:
         n = sc.nextInt();
         k = sc.nextInt();
-        for(int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
             arr[i] = sc.nextInt();
 
         int ans = 0;
 
         // 배열을 앞에서부터 순회하며 쌍을 만들어줍니다.
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             long diff = k - arr[i];
             // 가능한 모든 쌍의 수를 세어줍니다.
-            if(count.containsKey(diff))
+            if (count.containsKey(diff))
                 ans += count.get(diff);
 
             // 현재 숫자의 개수를 하나 증가시켜줍니다.
-            if(!count.containsKey(arr[i]))
-                count.put(arr[i], 1);
-            else
-                count.put(arr[i], count.get(arr[i]) + 1);
+
+            count.put(       arr[i], count.getOrDefault(arr[i],0)+1       );
+            
+       
+           
         }
 
         System.out.print(ans);
