1a7199ac5e6c84f42a823abc6f3827ead8478c7f
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-05-21 19:06:51 +0900

    Title: 씨 오 더블유, Time: 131ms, Memory: 11MB, Status: Wrong Answer - Codetree

---
+++ 250521/씨 오 더블유/c-o-w.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s = sc.next();
+        s = "K" + s;
+        char[] c = s.toCharArray();
+
+        int[] cl = new int[n + 1];
+        int[] wr = new int[n + 2];
+
+        for (int i = 1; i <= n; i++) {
+            if (c[i] == 'C') {
+                cl[i] = cl[i - 1] + 1;
+            } else {
+                cl[i] = cl[i - 1];
+            }
+
+        }
+
+        for (int i = n; i > 0; i--) {
+            if (c[i] == 'W') {
+                wr[i] = wr[i + 1] + 1;
+            } else {
+                wr[i] = wr[i + 1];
+            }
+
+        }
+
+
+        int res = 0;
+
+        for (int i = 2; i < n; i++) {
+            if (c[i] == 'O') {
+                res += cl[i] * wr[i];
+            }
+
+        }
+
+
+        System.out.println(res);
+
+
+    }
+}
