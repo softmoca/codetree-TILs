d19c602b83d380974617b994d029c1dc7e0a4904
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-05-22 12:57:47 +0900

    Title: 최대 구간의 크기, Time: 1837ms, Memory: 60MB, Status: Passed - Codetree

---
+++ 250522/최대 구간의 크기/size-of-largest-interval.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static class Pair {
+        int x, v;
+
+        public Pair(int x, int v) {
+            this.x = x;
+            this.v = v;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        List<Pair> pairs = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int x1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            pairs.add(new Pair(x1, +1));
+            pairs.add(new Pair(x2, -1));
+        }
+
+        pairs.sort(
+                Comparator.comparing((Pair p) -> p.x)
+        );
+
+
+        int res = 0;
+        int sum = 0;
+        int startX = 0;
+        int endX = 0;
+        for (Pair p : pairs) {
+            int x = p.x;
+            int v = p.v;
+
+            if (v == 1) {
+                if (sum == 0) {
+                    startX = x;
+                }
+
+                sum++;
+
+            } else {
+                sum--;
+                if (sum == 0) {
+                    endX = x;
+                    res = Math.max(endX - startX, res);
+                }
+            }
+
+        }
+
+        System.out.println(res);
+
+
+    }
+}
