e957690db1aaa1e5b4e76916672968f802764269
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-05-26 12:38:50 +0900

    Title: 바구니 안의 사탕, Time: 1201ms, Memory: 66MB, Status: Wrong Answer - Codetree

--- 250526/바구니 안의 사탕/candy-in-the-basket.java
+++ 250526/바구니 안의 사탕/candy-in-the-basket.java
@@ -3,7 +3,6 @@ import java.util.*;
 
 public class Main {
 
-
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -12,32 +11,30 @@ public class Main {
 
         int sum = 0;
 
-
+        // 입력 처리
         while (n-- > 0) {
             int x = sc.nextInt();
             int y = sc.nextInt();
-            arr[y] = arr[y] + x;
+            arr[y] += x;
         }
 
+        // 초기 구간 합 계산 (1 ~ 2k+1)
         for (int i = 1; i <= 2 * k + 1; i++) {
             sum += arr[i];
         }
+
         int p1 = 1;
         int p2 = 2 * k + 2;
         int res = sum;
-        while (true) {
-            if (p2 == 1000000) break;
 
-            // System.out.println(p1 + " " + p2);
-
-            sum = sum + arr[p2++] - arr[p1++];
-            //   System.out.println(sum);
+        // 슬라이딩 윈도우 수행
+        while (p2 < arr.length) {  // 안전하게 배열 끝까지 접근
+            sum = sum + arr[p2] - arr[p1];
+            p2++;
+            p1++;
             res = Math.max(res, sum);
-
-
         }
+
         System.out.println(res);
-
-
     }
 }
