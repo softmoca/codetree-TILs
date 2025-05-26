41092be1643e84a042ef8fcc98e1b0a967c8198d
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-05-26 10:44:40 +0900

    Title: 가장 짧은 부분합, Time: 615ms, Memory: 33MB, Status: Wrong Answer - Codetree

--- 250526/가장 짧은 부분합/shortest-subtotal.java
+++ 250526/가장 짧은 부분합/shortest-subtotal.java
@@ -2,8 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -13,34 +11,28 @@ public class Main {
             arr[i] = sc.nextInt();
         }
 
-
-        int j = 0;
+        int p1 = 1, p2 = 0;
         int sum = 0;
         int res = Integer.MAX_VALUE;
 
-        for (int i = 1; i <= n; i++) {
-
-            while (j + 1 <= n && arr[j + 1] + sum <= s) {
-                j++;
-                sum += arr[j];
+        while (p1 <= n) {
+            // 오른쪽 포인터를 옮기며 합이 s 이하인 최대 구간을 찾음
+            while (p2 + 1 <= n && sum + arr[p2 + 1] <= s) {
+                p2++;
+                sum += arr[p2];
             }
-            if (j + 1 == n && sum < s) break;
+
+            if (p2 + 1 == n && sum < s) break;
 
             if (sum >= s) {
-                res = Math.min(res, j - i + 1);
+                res = Math.min(res, p2 - p1 + 1);
             }
-            System.out.println(j + " " + i + " " + sum);
 
-
-            sum -= arr[i];
-
+            // 왼쪽 포인터 이동 (구간 축소)
+            sum -= arr[p1];
+            p1++;
         }
 
         System.out.println(res);
-
-
     }
-
-
 }
-
