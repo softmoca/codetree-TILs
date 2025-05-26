87145c0d4faba115702b6bf228b43558f47fbf21
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-05-26 12:40:20 +0900

    Title: 바구니 안의 사탕, Time: 1208ms, Memory: 69MB, Status: Passed - Codetree

--- 250526/바구니 안의 사탕/candy-in-the-basket.java
+++ 250526/바구니 안의 사탕/candy-in-the-basket.java
@@ -3,41 +3,39 @@ import java.util.*;
 
 public class Main {
 
-
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int k = sc.nextInt();
-        int[] arr = new int[4000006];
 
-        int sum = 0;
-
+        final int MAX_POS = 1000000;
+        int[] arr = new int[MAX_POS + 2 * k + 5];  // 충분한 크기 확보
 
         while (n-- > 0) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-            arr[y] = arr[y] + x;
+            int x = sc.nextInt(); // 사탕 개수
+            int y = sc.nextInt(); // 위치
+            arr[y] += x;
         }
 
-        for (int i = 1; i <= 2 * k + 1; i++) {
+        int windowSize = 2 * k + 1;
+        long sum = 0;
+
+        // ✅ 초기 구간: 0 ~ 2k
+        for (int i = 0; i <= 2 * k; i++) {
             sum += arr[i];
         }
-        int p1 = 1;
-        int p2 = 2 * k + 2;
-        int res = sum;
-        while (true) {
-            if (p2 == 4000006) break;
 
-            // System.out.println(p1 + " " + p2);
+        int p1 = 0;
+        int p2 = 2 * k;
+        long res = sum;
 
-            sum = sum + arr[p2++] - arr[p1++];
-            //   System.out.println(sum);
+        while (p2 + 1 < arr.length) {
+            p1++;
+            p2++;
+            sum = sum + arr[p2] - arr[p1 - 1];
             res = Math.max(res, sum);
-
-
         }
+
         System.out.println(res);
-
-
     }
 }
