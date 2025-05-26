2172f507e73abab9977b6016aac412fd051e0013
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-05-26 10:38:12 +0900

    Title: 부분수열 여부 판단하기, Time: 126ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250526/부분수열 여부 판단하기/determine-subsequence.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int[] arr2 = new int[m + 1];
+
+        for (int i = 1; i <= m; i++) {
+            arr2[i] = sc.nextInt();
+        }
+
+
+        int p1 = 1, p2 = 1;
+
+        while (true) {
+           // System.out.println(p1 + " " + p2);
+            if (arr[p1] == arr2[p2]) {
+                p1++;
+                p2++;
+            } else {
+                p1++;
+            }
+
+            if (p1 == n + 1) {
+                System.out.println("No");
+                break;
+            } else if (p2 == m + 1) {
+                System.out.println("Yes");
+
+                break;
+            }
+
+
+        }
+
+
+    }
+}
