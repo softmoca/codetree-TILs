830809af1cf94bd9682608258b4e64ce7fcb9188
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-05-26 12:50:22 +0900

    Title: 바구니 안의 사탕, Time: 1384ms, Memory: 55MB, Status: Passed - Codetree

--- 250526/바구니 안의 사탕/candy-in-the-basket.java
+++ 250526/바구니 안의 사탕/candy-in-the-basket.java
@@ -1,37 +1,57 @@
 import java.util.Scanner;
+import java.util.Arrays;
+
+class Candy implements Comparable<Candy> {
+    int x, cnt;
+
+    public Candy(int x, int cnt) {
+        this.x = x;
+        this.cnt = cnt;
+    }
+
+    @Override
+    public int compareTo(Candy c) {
+        return this.x - c.x; // 위치 기준 오름차순 정렬
+    }
+}
 
 public class Main {
+    public static final int MAX_N = 100000;
+    public static Candy[] candies = new Candy[MAX_N + 1];
+    public static int n, k;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        k = sc.nextInt();
 
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-
-        int[] arr = new int[1000001];  // 좌표 범위: 0 ~ 1_000_000
-
-        // 입력: 위치에 사탕 누적
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             int cnt = sc.nextInt();
             int x = sc.nextInt();
-            arr[x] += cnt;
+            candies[i] = new Candy(x, cnt);
         }
 
-        int windowSize = 2 * k + 1;
-        long sum = 0;
-        long max = 0;
+        // 위치 기준 정렬
+        Arrays.sort(candies, 1, n + 1);
 
-        // 초기 구간 [0, windowSize - 1]
-        for (int i = 0; i < windowSize && i < arr.length; i++) {
-            sum += arr[i];
+        // 슬라이딩 윈도우
+        int p1 = 1, p2 = 1;
+        int sum = 0;
+        int ans = 0;
+
+        while (p2 <= n) {
+            // 윈도우 오른쪽 확장: [candies[p1].x, candies[p2].x] ≤ 2k 범위
+            if (candies[p2].x - candies[p1].x <= 2 * k) {
+                sum += candies[p2].cnt;
+                ans = Math.max(ans, sum);
+                p2++;
+            } else {
+                // 왼쪽 윈도우 수축
+                sum -= candies[p1].cnt;
+                p1++;
+            }
         }
-        max = sum;
 
-        // 슬라이딩 윈도우: 왼쪽을 하나 빼고 오른쪽을 하나 더함
-        for (int i = windowSize; i < arr.length; i++) {
-            sum += arr[i] - arr[i - windowSize];
-            max = Math.max(max, sum);
-        }
-
-        System.out.println(max);
+        System.out.println(ans);
     }
 }
