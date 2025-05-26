373a6244ae7ce44500bd007031d139ab1847b234
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-05-26 12:23:30 +0900

    Title: 바구니 안의 사탕, Time: 940ms, Memory: 54MB, Status: Runtime Error - Codetree

---
+++ 250526/바구니 안의 사탕/candy-in-the-basket.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[1000001];
+
+        int sum = 0;
+
+
+        while (n-- > 0) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            arr[y] = x;
+        }
+
+        for (int i = 1; i <= 2 * k + 1; i++) {
+            sum += arr[i];
+        }
+        int p1 = 1;
+        int p2 = 2 * k + 2;
+        int res = sum;
+        while (true) {
+            if (p2 == 1000000) break;
+
+            // System.out.println(p1 + " " + p2);
+
+            sum = sum + arr[p2++] - arr[p1++];
+            //   System.out.println(sum);
+            res = Math.max(res, sum);
+
+
+        }
+        System.out.println(res);
+
+
+    }
+}
