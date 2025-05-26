df17f7d6f3e35d27e066306b5314b7c5e42f1aae
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-05-26 12:49:54 +0900

    Title: 바구니 안의 사탕, Time: 878ms, Memory: 54MB, Status: Passed - Codetree

--- 250526/바구니 안의 사탕/candy-in-the-basket.java
+++ 250526/바구니 안의 사탕/candy-in-the-basket.java
@@ -1,76 +1,37 @@
 import java.util.Scanner;
-import java.util.Arrays;
-
-class Candy implements Comparable<Candy> {
-    int x, cnt;
-
-    public Candy(int x, int cnt) {
-        this.x = x;
-        this.cnt = cnt;
-    }
-
-    @Override
-    public int compareTo(Candy c) {
-        return this.x - c.x;        // x 기준 오름차순 정렬
-    }
-}
 
 public class Main {
-    public static final int MAX_N = 100000;
-    
-    // 변수 선언
-    public static Candy[] candies = new Candy[MAX_N + 1];
-    public static int n, k;
-    
-    // 해당 내용물의 위치를 반환합니다.
-    public static int getPosOfCandy(int candyIdx) {
-        return candies[candyIdx].x;
-    }
-    
-    // 해당 내용물에 들어 있는 사탕 수를 반환합니다.
-    public static int getNumOfCandy(int candyIdx) {
-        return candies[candyIdx].cnt;
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // 입력:
-        n = sc.nextInt();
-        k = sc.nextInt();
-        for(int i = 1; i <= n; i++) {
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] arr = new int[1000001];  // 좌표 범위: 0 ~ 1_000_000
+
+        // 입력: 위치에 사탕 누적
+        for (int i = 0; i < n; i++) {
             int cnt = sc.nextInt();
             int x = sc.nextInt();
-            candies[i] = new Candy(x, cnt);
+            arr[x] += cnt;
         }
 
-        // x순으로 오름차순 정렬해줍니다.
-        Arrays.sort(candies, 1, n + 1);
+        int windowSize = 2 * k + 1;
+        long sum = 0;
+        long max = 0;
 
-        // 가능한 구간 중 최대 사탕의 수를 구합니다.
-        int ans = 0;
-        
-        // 구간을 잡아봅니다.
-        // 구간 내에 있는 사탕의 수를 계속 계산하여 관리해줍니다.
-        int totalNums = 0;
-        int j = 0;
-        for(int i = 1; i <= n; i++) {
-            // 구간의 크기가 2K보다 같거나 작은 경우에 한하여 최대로 진행합니다.
-            while(j + 1 <= n && getPosOfCandy(j + 1) - getPosOfCandy(i) <= 2 * k) {
-                totalNums += getNumOfCandy(j + 1);
-                j++;
-            }
-            
-            // 현재 구간 [i, j]는 
-            // i를 시작점으로 하는
-            // 가장 긴 구간이므로
-            // 구간 내 최대 사탕의 수를 갱신해줍니다.
-            ans = Math.max(ans, totalNums);
+        // 초기 구간 [0, windowSize - 1]
+        for (int i = 0; i < windowSize && i < arr.length; i++) {
+            sum += arr[i];
+        }
+        max = sum;
 
-            // 다음 구간으로 넘어가기 전에
-            // i번째에 해당하는 사탕을 구간에서 제외시킵니다.
-            totalNums -= getNumOfCandy(i);
+        // 슬라이딩 윈도우: 왼쪽을 하나 빼고 오른쪽을 하나 더함
+        for (int i = windowSize; i < arr.length; i++) {
+            sum += arr[i] - arr[i - windowSize];
+            max = Math.max(max, sum);
         }
 
-        System.out.print(ans);
+        System.out.println(max);
     }
 }
