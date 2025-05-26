1740748b79a516ceabfb404f85753ae809d7e16b
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-05-26 12:34:09 +0900

    Title: 바구니 안의 사탕, Time: 1107ms, Memory: 65MB, Status: Runtime Error - Codetree

--- 250526/바구니 안의 사탕/candy-in-the-basket.java
+++ 250526/바구니 안의 사탕/candy-in-the-basket.java
@@ -8,7 +8,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int k = sc.nextInt();
-        int[] arr = new int[k*2+1];
+        int[] arr = new int[4000006];
 
         int sum = 0;
 
