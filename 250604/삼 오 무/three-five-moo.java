04955aa99ead8c95d4d4b04d67edfa28ad9ad2fa
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-06-04 11:05:03 +0900

    Title: 삼 오 무, Time: 2934ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250604/삼 오 무/three-five-moo.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        long n = 1000000000L;
+        long cnt = 0;
+        for (long i = 1; i <= n; i++) {
+            if (i % 3 == 0 || i % 5 == 0) continue;
+            cnt++;
+            if (cnt == x) {
+                System.out.println(i);
+                break;
+            }
+
+
+        }
+    }
+}
