8a18d5ee1db84a25d60099d62545cb434311ecfc
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-06-04 11:05:36 +0900

    Title: 삼 오 무, Time: 3014ms, Memory: 10MB, Status: Time Limit Exceed - Codetree

--- 250604/삼 오 무/three-five-moo.java
+++ 250604/삼 오 무/three-five-moo.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
-        long n = 1000000000L;
+        long n = 10000000000L;
         long cnt = 0;
         for (long i = 1; i <= n; i++) {
             if (i % 3 == 0 || i % 5 == 0) continue;
