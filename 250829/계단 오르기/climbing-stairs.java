78dd0af945357bdf7705ae3a837748294f9d3fcf
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-08-29 09:55:54 +0900

    Title: 계단 오르기, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

--- 250829/계단 오르기/climbing-stairs.java
+++ 250829/계단 오르기/climbing-stairs.java
@@ -8,7 +8,7 @@ public class Main {
         dp[2] = 1;
         dp[3] = 1;
         for (int i = 4; i < 1001; i++) {
-            dp[i] = dp[i - 2] + dp[i - 3];
+            dp[i] = (dp[i - 2] + dp[i - 3])%10007;
         }
 
         System.out.println(dp[n]);
