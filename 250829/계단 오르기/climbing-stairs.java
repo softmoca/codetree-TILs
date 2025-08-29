5bb83a11f37265c248e8af0d790ed5201b7f2125
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-08-29 09:53:22 +0900

    Title: 계단 오르기, Time: 124ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250829/계단 오르기/climbing-stairs.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] dp = new int[1001];
+        dp[2] = 1;
+        dp[3] = 1;
+        for (int i = 4; i < 1001; i++) {
+            dp[i] = dp[i - 2] + dp[i - 3];
+        }
+
+        System.out.println(dp[n]);
+
+
+    }
+}
