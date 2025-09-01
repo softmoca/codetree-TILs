7a6acb66433a5085bc1e11a8c39aaf3ca8e8210d
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-09-01 17:03:18 +0900

    Title: 동전 거슬러주기, Time: 89ms, Memory: 9MB, Status: Runtime Error - Codetree

---
+++ 250901/동전 거슬러주기/coin-change.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+/*
+
+ */
+
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        int[] dp = new int[m + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[arr[i]] = 1;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i - arr[j] >= 0 && dp[i - arr[j]] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i - arr[j]] + 1);
+                }
+            }
+        }
+        if (dp[m] == Integer.MAX_VALUE) {
+            System.out.println(-1);
+        } else {
+            System.out.println(dp[m]);
+        }
+
+
+    }
+}
