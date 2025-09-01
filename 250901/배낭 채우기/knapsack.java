68c0213e7e731254537579630db748a82377b5ce
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-09-01 18:24:22 +0900

    Title: 배낭 채우기, Time: 89ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250901/배낭 채우기/knapsack.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+/*
+1. 1차원 배열로 풀기 -> 모든 배열중 최대값 -> 끝값배로 출력 비교
+2. 2차원배열로 풀기
+
+ */
+
+
+public class Main {
+    
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] w=new int[n+1];
+        int[] v=new int[n+1];
+        for(int i=1;i<=n;i++){
+            st = new StringTokenizer(br.readLine());
+            
+            w[i]=Integer.parseInt(st.nextToken());
+            v[i]=Integer.parseInt(st.nextToken());
+        }
+        int[] dp=new int[m+1];
+        Arrays.fill(dp,-1);
+        dp[0]=0;
+        
+        
+        for(int i=1;i<=n;i++){
+            for(int j=m;j>=0;j--){
+                if(j-w[i]<0) continue;
+                if(dp[j-w[i]]==-1) continue;
+                dp[j]=Math.max(dp[j],dp[j-w[i]]+v[i]);
+            }
+        }
+        
+        int res=-1;
+        for(int i=1;i<=m;i++){
+            res=Math.max(res,dp[i]);
+        }
+
+        System.out.println(res);
+        
+
+    }
+}
