40994c2237186eaa0bf8d5a6471f678f2acb15a3
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-09-02 12:26:50 +0900

    Title: 최대 점프 횟수, Time: 94ms, Memory: 9MB, Status: Passed - Codetree

--- 250902/최대 점프 횟수/maximum-number-of-jumps.java
+++ 250902/최대 점프 횟수/maximum-number-of-jumps.java
@@ -7,35 +7,37 @@ import java.util.*;
 
 
 public class Main {
-    
-    
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+
         int n = Integer.parseInt(br.readLine());
-        int[] arr=new int[n];
+        int[] arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        
-        int[] dp=new int[n];
+
+        int[] dp = new int[n];
         Arrays.fill(dp, -1);
-        dp[0]=0;
-        for(int i=0;i<n-1;i++){
-            if(dp[i]==-1) continue;
-            int cnt=arr[i];
-            
-            for(int j=1;j<=cnt;j++){
-                dp[i+j]=Math.max(dp[i+j],dp[i]+1);
+        dp[0] = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (dp[i] == -1) continue;
+            int cnt = arr[i];
+
+            for (int j = 1; j <= cnt; j++) {
+                if(i+j>=n) continue;
                 
-                
+                dp[i + j] = Math.max(dp[i + j], dp[i] + 1);
+
+
             }
-            
+
         }
-        int res=0;
-        for(int i=0;i<n;i++){
-            res=Math.max(res,dp[i]);
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            res = Math.max(res, dp[i]);
         }
         System.out.println(res);
 
