44f6aac9bcb4d6fd5370dc427e843b8999f7dfbf
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-09-02 13:05:26 +0900

    Title: 증가했다가 감소하는 부분 수열, Time: 128ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250902/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+/*
+
+ */
+
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int[] dp1 = new int[n];
+        int[] dp2 = new int[n];
+        Arrays.fill(dp1, 1);
+        Arrays.fill(dp2, 1);
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp1[i] = Math.max(dp1[i], dp1[j] + 1);
+                }
+            }
+        }
+
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] > arr[j]) {
+                    dp2[i] = Math.max(dp2[i], dp2[j] + 1);
+                }
+            }
+        }
+        
+        
+        int res=0;
+        for (int i = 0; i < n; i++) {
+            res=Math.max(res,dp1[i] + dp2[i]);
+            
+        }
+        System.out.println(res-1);
+
+
+    }
+}
