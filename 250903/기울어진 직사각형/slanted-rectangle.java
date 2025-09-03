504257e0bfea77ab62b1336fd848f75e3fe64d7e
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-09-03 17:48:28 +0900

    Title: 기울어진 직사각형, Time: 190ms, Memory: 15MB, Status: Passed - Codetree

--- 250903/기울어진 직사각형/slanted-rectangle.java
+++ 250903/기울어진 직사각형/slanted-rectangle.java
@@ -1,91 +1,60 @@
-import java.io.*;
-import java.util.*;
-
-/*
-
- */
-
+import java.util.Scanner;
 
 public class Main {
-
-    static int res = 0;
-
-    static void check(int x, int y, int cnt1, int cnt2, int n, int[][] arr) {
-        int[] dx = {-1, -1, 1, 1};
-        int[] dy = {1, -1, -1, 1};
-        int sum = 0;
-
-        int nx = -1;
-        int ny = -1;
-        for (int i = 0; i < cnt1; i++) {
-            nx = x + dx[0];
-            ny = y + dy[0];
-            if (nx < 0 || ny < 0 || nx >= n || ny >= n) return;
-            x = nx;
-            y = ny;
-            sum += arr[x][y];
-
-        }
-
-        for (int i = 0; i < cnt2; i++) {
-            nx = x + dx[1];
-            ny = y + dy[1];
-            if (nx < 0 || ny < 0 || nx >= n || ny >= n) return;
-            x = nx;
-            y = ny;
-            sum += arr[x][y];
-        }
-
-        for (int i = 0; i < cnt1; i++) {
-            nx = x + dx[2];
-            ny = y + dy[2];
-            if (nx < 0 || ny < 0 || nx >= n || ny >= n) return;
-            x = nx;
-            y = ny;
-            sum += arr[x][y];
-        }
-
-        for (int i = 0; i < cnt2; i++) {
-            nx = x + dx[3];
-            ny = y + dy[3];
-            if (nx < 0 || ny < 0 || nx >= n || ny >= n) return;
-            x = nx;
-            y = ny;
-            sum += arr[x][y];
-        }
-
-res=Math.max(res,sum);
+    public static final int DIR_NUM = 4;
+    public static final int MAX_N = 20;
+    
+    public static int n;
+    public static int[][] grid = new int[MAX_N][MAX_N];
+    
+    public static boolean inRange(int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < n;
+    }
+    
+    public static int getScore(int x, int y, int k, int l) {
+        int[] dx = new int[]{-1, -1, 1, 1};
+        int[] dy = new int[]{1, -1, -1, 1};
+        int[] moveNum = new int[]{k, l, k, l};
+        
+        int sumOfNums = 0;
+    
+        // 기울어진 직사각형의 경계를 쭉 따라가봅니다.
+        for(int d = 0; d < DIR_NUM; d++)
+            for(int q = 0; q < moveNum[d]; q++) {
+                x += dx[d]; y += dy[d];
+                    
+                // 기울어진 직사각형이 경계를 벗어나는 경우라면
+                // 불가능하다는 의미로 답이 갱신되지 않도록
+                // 0을 반환합니다.
+                if(!inRange(x, y))
+                    return 0;
+                
+                sumOfNums += grid[x][y];
+            }
+        
+        return sumOfNums;
     }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                grid[i][j] = sc.nextInt();
+        
+        int ans = 0;
+        
+        // (i, j)를 시작으로 1, 2, 3, 4 방향
+        // 순서대로 길이 [k, l, k, l] 만큼 이동하면 그려지는
+        // 기울어진 직사각형을 잡아보는
+        // 완전탐색을 진행해봅니다.
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                for(int k = 1; k < n; k++)
+                    for(int l = 1; l < n; l++)
+                        ans = Math.max(ans, getScore(i, j, k, l));
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int k = 2; k < n; k++) {
-                    for (int num1 = 1; num1 < k; num1++) {
-                        //          System.out.println(num1 + " " + (k - num1));
-                        check(i, j, num1, k - num1, n, arr);
-                    }
-
-
-                }
-
-
-            }
-        }
-        System.out.println(res);
-
+        System.out.print(ans);
     }
 }
