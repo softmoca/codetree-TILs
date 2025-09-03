cbc23a2a5a91d97b4af88f64eca9accb87d133ec
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-09-03 11:36:35 +0900

    Title: 금 채굴하기, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

--- 250903/금 채굴하기/gold-mining.java
+++ 250903/금 채굴하기/gold-mining.java
@@ -6,35 +6,32 @@ public class Main {
     public static int n, m;
     public static int[][] grid = new int[MAX_NUM][MAX_NUM];
     
-    // 주어진 k에 대하여 마름모의 넓이를 반환합니다.
     public static int getArea(int k) {
         return k * k + (k+1) * (k+1); 
     }
     
-    // 주어진 좌표가 격자에 포함되는지 여부를 반환합니다.
     public static boolean inRange(int x, int y) {
         return x >= 0 && x < n && y >= 0 && y < n;
     }
     
-    // 주어진 k에 대하여 채굴 가능한 금의 개수를 반환합니다.
-    public static int getNumOfGold(int row, int col, int k) {
+    // 주어진 k에 대하여 모서리에서 채굴 가능한 금의 개수를 반환합니다.
+    public static int getNumOfGoldInBorder(int row, int col, int k) {
         int numOfGold = 0;
         int[] dx = new int[]{1, 1, -1, -1};
         int[] dy = new int[]{-1, 1, 1, -1}; // 방향에 따라 바뀌는 x와 y의 변화량을 정의합니다.
     
-        numOfGold += grid[row][col]; // k=0 일 때 처리
+        if(k == 0)
+            return grid[row][col];
     
-        for(int currK = 1; currK <= k; currK++) {
-            int currX = row - currK, currY = col; // 순회 시작점 설정
+        int currX = row - k, currY = col; // 순회 시작점 설정
     
-            for(int currDir = 0; currDir < 4; currDir++) {
-                for(int step = 0; step < currK; step++) {
-                    if(inRange(currX, currY)) {
-                        numOfGold += grid[currX][currY];
-                    }
-                    currX = currX + dx[currDir];
-                    currY = currY + dy[currDir];
+        for(int currDir = 0; currDir < 4; currDir++) {
+            for(int step = 0; step < k; step++) {
+                if(inRange(currX, currY)) {
+                    numOfGold += grid[currX][currY];
                 }
+                currX = currX + dx[currDir];
+                currY = currY + dy[currDir];
             }
         }
     
@@ -54,8 +51,11 @@ public class Main {
 
         for(int row = 0; row < n; row++) {
             for(int col = 0; col < n; col++) {
+                int numOfGold = 0;
                 for(int k = 0; k <= 2 * (n-1); k++) {
-                    int numOfGold = getNumOfGold(row, col, k);
+                    // 이전 k까지 구한 금의 개수에
+                    // 해당 k의 모서리에 존재하는 금의 개수를 더해줍니다.
+                    numOfGold += getNumOfGoldInBorder(row, col, k);
 
                     if(numOfGold * m >= getArea(k))
                         maxGold = Math.max(maxGold, numOfGold);
