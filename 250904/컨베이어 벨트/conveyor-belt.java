14a0e4731d5cec391eee4b3fc94dbac17b8cc0cb
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-09-04 09:51:18 +0900

    Title: 컨베이어 벨트, Time: 114ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250904/컨베이어 벨트/conveyor-belt.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+/*
+
+ */
+
+
+public class Main {
+    
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n=Integer.parseInt(st.nextToken());
+        int t=Integer.parseInt(st.nextToken());
+        Deque<Integer> q=new ArrayDeque<>();
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            q.addLast(Integer.parseInt(st.nextToken()));
+        }
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            q.addLast(Integer.parseInt(st.nextToken()));
+        }
+        
+        while(t-->0){
+            q.addFirst(q.removeLast());
+        }
+        
+        for(int i=0;i<n;i++){
+            System.out.print(q.removeFirst()+" ");
+        }
+        System.out.println();
+
+        for(int i=0;i<n;i++){
+            System.out.print(q.removeFirst()+" ");
+        }
+        
+
+    }
+}
