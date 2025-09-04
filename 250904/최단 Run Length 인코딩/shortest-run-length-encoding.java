44fd6bbc41e5a5035ef1b603a38c5f8098d4c60e
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-09-04 12:08:20 +0900

    Title: 최단 Run Length 인코딩, Time: 87ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- 250904/최단 Run Length 인코딩/shortest-run-length-encoding.java
+++ 250904/최단 Run Length 인코딩/shortest-run-length-encoding.java
@@ -7,26 +7,42 @@ import java.util.*;
 
 
 public class Main {
+    static int res = 10000;
 
+    static void go(String input) {
+
+        int cnt = 1;
+        String temp = "";
+
+        for (int i = 1; i < input.length(); i++) {
+            if (input.charAt(i) == input.charAt(i - 1)) {
+                cnt++;
+            } else {
+                temp = temp + input.charAt(i - 1);
+                temp = temp + String.valueOf(cnt);
+                cnt = 1;
+            }
+        }
+
+        temp = temp + input.charAt(input.length() - 1);
+        temp = temp + String.valueOf(cnt);
+        res = Math.min(res, temp.length());
+        //System.out.println(temp.length());
+
+
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String input = br.readLine();
-        int res = 0;
+
 
         for (int i = 1; i < input.length(); i++) {
             input = input.substring(1, input.length()) + input.charAt(0);
-            Set<Character> set = new HashSet<>();
 
-            for (char c : input.toCharArray()) {
-                set.add(c);
-            }
-            if (input.length() == 10 && set.size() == 1) {
-                res = 3;
-            }
 
-            res = Math.max(res, set.size() * 2);
+            go(input);
 
 
         }
