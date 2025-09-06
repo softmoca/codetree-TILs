2358a958f43681818396cd9bd8de18e67a6a8dc8
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-09-06 22:41:17 +0900

    Title: 2차원 폭발 게임, Time: 1236ms, Memory: 176MB, Status: Memory Limit Exceed - Codetree

--- 250906/2차원 폭발 게임/The-2D-bomb-game.java
+++ 250906/2차원 폭발 게임/The-2D-bomb-game.java
@@ -15,63 +15,32 @@ public class Main {
     static int n, m, k;
     static int[][] arr;
 
-//    static void boom(int col) {
-//        int cnt = 1;
-//        for (int i = 1; i < n; i++) {
-//            if (arr[i][col] != 0 && arr[i - 1][col] == arr[i][col]) {
-//                cnt++;
-//            } else {
-//                if (cnt >= m && arr[i - 1][col] != 0) {  // 0이 아닐 때만 제거
-//                    for (int j = 0; j < cnt; j++) {
-//                        arr[i - 1 - j][col] = 0;
-//                    }
-//                }
-//                cnt = 1;
-//            }
-//        }
-//
-//        // 마지막 그룹 처리
-//        if (cnt >= m && arr[n - 1][col] != 0) {  // 0이 아닐 때만 제거
-//            for (int j = 0; j < cnt; j++) {
-//                arr[n - 1 - j][col] = 0;
-//            }
-//        }
-//    }
+    static void boom(int col) {
 
-    // 1) 0은 제외하고 세기 + 변화 여부 리턴
-    static boolean boom(int col) {
-        boolean changed = false;
         int cnt = 1;
         for (int i = 1; i < n; i++) {
-            if (arr[i][col] != 0 && arr[i - 1][col] == arr[i][col]) cnt++;
-            else {
-                if (cnt >= m && arr[i - 1][col] != 0) {
-                    changed = true;
-                    for (int j = 0; j < cnt; j++) arr[i - 1 - j][col] = 0;
+            if (arr[i - 1][col] == arr[i][col]) {
+                cnt++;
+            } else {
+                if (cnt >= m) {
+                    for (int j = 0; j < cnt; j++) {
+                        arr[i - 1 - j][col] = 0;
+                    }
                 }
                 cnt = 1;
             }
-        }
-        if (cnt >= m && arr[n - 1][col] != 0) {
-            changed = true;
-            for (int j = 0; j < cnt; j++) arr[n - 1 - j][col] = 0;
-        }
-        return changed;
-    }
 
-    // 2) 한 방향에서 더 이상 터질 게 없을 때까지 안정화
-    static boolean stabilize() {
-        boolean any = false;
-        while (true) {
-            boolean roundBoom = false;
-            for (int c = 0; c < n; c++) roundBoom |= boom(c);
-            if (!roundBoom) break;
-            any = true;
-            for (int c = 0; c < n; c++) gravity(c);  // 터진 뒤 중력
-        }
-        return any; // 이번 안정화 단계에서 뭔가 터졌는지
-    }
 
+        }
+
+        if (cnt >= m) {
+            for (int j = 0; j < cnt; j++) {
+                arr[n - 1 - j][col] = 0;
+            }
+        }
+
+
+    }
 
     static void gravity(int col) {
         Deque<Integer> dq = new ArrayDeque<>();
@@ -131,9 +100,20 @@ public class Main {
 
         while (k-- > 0) {
 
-            boolean exploded = stabilize(); // 🔸여기서 연쇄 폭발 다 처리
-            // (문제 규칙에 따라) 아무것도 안 터지면 더 진행하지 않고 끝내고 싶다면:
-            // if (!exploded) break;
+
+            for (int i = 0; i < 100; i++) {
+
+                // 열 기준 m개 이상 연속 체크 후 제거
+                for (int col = 0; col < n; col++) {
+                    boom(col);
+                }
+
+                // 중력
+                for (int col = 0; col < n; col++) {
+                    gravity(col);
+                }
+
+            }
 
 
             //90도 회전
@@ -148,8 +128,20 @@ public class Main {
 
         }
 
-        // 최종 집계 전에 한 번 더 안정화
-        stabilize();
+        for (int i = 0; i < 100; i++) {
+
+            // 열 기준 m개 이상 연속 체크 후 제거
+            for (int col = 0; col < n; col++) {
+                boom(col);
+            }
+
+            // 중력
+            for (int col = 0; col < n; col++) {
+                gravity(col);
+            }
+
+        }
+
 
         int cnt = 0;
         for (int i = 0; i < n; i++) {
@@ -163,8 +155,6 @@ public class Main {
 
 
     }
-
-
 }
 /*
     for (int i = 0; i < n; i++) {
