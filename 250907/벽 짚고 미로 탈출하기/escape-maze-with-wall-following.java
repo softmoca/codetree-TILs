58fee1caef82d82cd013a264c9b6ad1b0365d10d
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-09-07 16:17:30 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 157ms, Memory: 11MB, Status: Passed - Codetree

--- 250907/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.java
+++ 250907/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.java
@@ -1,129 +1,117 @@
-import java.io.*;
-import java.util.*;
-
-/*
-바라보고있는 방향으로 이동 불가 -> 반시계 회전
-이동가능 1 - 밖인경우 -> 탈출
-이동가능 2 - 오른쪽 짚을 벽이있다 -> 한칸이동
-이동간으 3- 오른쪽 짚을 벽이 없다 -> 한칸이동 후 시ㅖ방향 회전 후 전진
-
-이동할때만 1초 소요
-
-
-
-1. 현재 방향으로 이동 가능한지 확인
-1-1 이동가능 ->
-1-2 이동 불가능 -> 반시계 방향 회전
-
-
-
-1. 이동 가능한 방향 찾기
-2. 이동
-    밖인경우
-    오른쪽 벽이있는경우
-    오른쪽 벽이 없는경우
- */
-
+import java.util.Scanner;
 
 public class Main {
-    static int n;
-    static char[][] arr;
-    static int startX;
-    static int startY;
-    static boolean[][][] visited;
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
-    static int dir = 1;
-
-
-    static int findD() {
-
-        for (int k = 4 + dir; k > dir; k--) {
-            int nx = startX + dx[k % 4];
-            int ny = startY + dy[k % 4];
-            if (nx >= 0 && ny >= 0 && nx < n && ny < n && arr[nx][ny] == '.') {
-                return k % 4;
-            } else if (nx < 0 || ny < 0 || nx >= n || ny >= n) {
-                return k % 4;
+    public static final int MAX_N = 100;
+    public static final int DIR_NUM = 4;
+    
+    // 전역 변수 선언:
+    public static int n;
+    
+    public static int currX, currY, currDir; // 현재 위치와 방향을 의미합니다.
+    public static char[][] a = new char[MAX_N + 1][MAX_N + 1];
+    
+    // 미로 탈출이 불가능한지 여부를 판단하기 위해
+    // 동일한 위치에 동일한 방향으로 진행했던 적이 있는지를
+    // 표시해주는 배열입니다.
+    public static boolean[][][] visited = new boolean[MAX_N + 1][MAX_N + 1][DIR_NUM];
+    
+    public static int elapsedTime;
+    
+    // 범위가 격자 안에 들어가는지 확인합니다.
+    public static boolean inRange(int x, int y) {
+        return 1 <= x && x <= n && 1 <= y && y <= n;
+    }
+    
+    // 해당 위치에 벽이 있으면 이동이 불가합니다.
+    public static boolean wallExist(int x, int y) {
+        return inRange(x, y) && a[x][y] == '#';
+    }
+    
+    // 조건에 맞춰 움직여봅니다.
+    public static void simulate() {
+        // 현재 위치에 같은 방향으로 진행한 적이 이미 있었는지 확인합니다.
+        // 이미 한 번 겪었던 상황이라면, 탈출이 불가능 하다는 의미이므로 
+        // -1을 출력하고 프로그램을 종료합니다.
+        if(visited[currX][currY][currDir]) {
+            System.out.print(-1);
+            System.exit(0);
+        }
+        // 현재 상황이 다시 반복되는지를 나중에 확인하기 위해
+        // 현재 상황에 해당하는 곳에 visited 값을 true로 설정합니다.
+        visited[currX][currY][currDir] = true;
+    
+        int[] dx = new int[]{0, 1, 0, -1};
+        int[] dy = new int[]{1, 0, -1, 0};
+    
+        int nextX = currX + dx[currDir];
+        int nextY = currY + dy[currDir];
+    
+        // Step1
+        
+        // 바라보고 있는 방향으로 이동하는 것이 불가능한 경우에는
+        // 반 시계 방향으로 90' 방향을 바꿉니다.
+        if(wallExist(nextX, nextY))
+            currDir = (currDir - 1 + 4) % 4;
+        
+        // Step2
+        
+        // Case1
+        // 바라보고 있는 방향으로 이동하는 것이 가능한 경우 중
+        // 바로 앞이 격자 밖이라면 탈출합니다.
+        else if(!inRange(nextX, nextY)) {
+            currX = nextX; currY = nextY;
+            elapsedTime++;
+        }
+    
+        // Case2 & Case3
+        // 바로 앞이 격자 안에서 이동할 수 있는 곳이라면 
+        else {
+            // 그 방향으로 이동했다 가정했을 때 바로 오른쪽에 짚을 벽이 있는지 봅니다.
+            int rx = nextX + dx[(currDir + 1) % 4];
+            int ry = nextY + dy[(currDir + 1) % 4];
+    
+            // Case2
+            // 그대로 이동해도 바로 오른쪽에 짚을 벽이 있다면 
+            // 해당 방향으로 한 칸 이동합니다.
+            if(wallExist(rx, ry)) {
+                currX = nextX; currY = nextY;
+                elapsedTime++;
+            }
+    
+            // Case3
+            // 그렇지 않다면 2칸 이동후 방향을 시계방향으로 90' 방향을 바꿉니다.
+            else {
+                currX = rx; currY = ry;
+                currDir = (currDir + 1) % 4;
+                elapsedTime += 2;
             }
         }
-
-        return -1;
     }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
+        currX = sc.nextInt();
+        currY = sc.nextInt();
+        
+        // 처음에는 우측 방향을 바라보고 시작합니다.
+        currDir = 0;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        startX = Integer.parseInt(st.nextToken()) - 1;
-        startY = Integer.parseInt(st.nextToken()) - 1;
-        arr = new char[n][n];
-        visited = new boolean[n][n][4];
-        visited[startX][startY][dir] = true;
-        for (int i = 0; i < n; i++) {
-            String input = br.readLine();
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = input.charAt(j);
-            }
+        for(int i = 1; i <= n; i++) {
+            String str = sc.next();
+            for(int j = 1; j <= n; j++)
+                a[i][j] = str.charAt(j - 1);
         }
+        
+        do {
+            // 조건에 맞춰 움직여봅니다.
+            simulate();
+        
+        // 격자를 빠져나오기 전까지 계속 반복합니다.
+        } while(inRange(currX, currY)); 
 
-        int time = 0;
-
-
-        while (true) {
-
-            dir = findD();
-
-            if (dir == -1) {
-                time = -1;
-                break;
-            }
-
-            startX = startX + dx[dir];
-            startY = startY + dy[dir];
-            if (startX < 0 || startY < 0 || startX >= n || startY >= n) {// 탈출
-                time++;
-                break;
-            }
-
-            if (visited[startX][startY][dir]) {
-                time = -1;
-                break;
-
-            } else {
-                visited[startX][startY][dir] = true;
-            }
-
-
-            int newDir = (dir + 1) % 4;
-            int tempX = startX + dx[newDir];
-            int tempY = startY + dy[newDir];
-            if (arr[tempX][tempY] == '.') {// 오른쪾 벽이 없는 경우
-                startX = tempX;
-                startY = tempY;
-                time++;
-                dir = newDir;
-                if (visited[startX][startY][dir]) {
-                    time = -1;
-                    break;
-
-                } else {
-                    visited[startX][startY][dir] = true;
-                }
-                
-            }
-
-         
-
-
-            time++;
-
-        }
-
-
-        System.out.println(time);
-
-
+        // 출력:
+        System.out.print(elapsedTime);
     }
 }
