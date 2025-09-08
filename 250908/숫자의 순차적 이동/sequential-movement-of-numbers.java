bcab210cefab8572a56289cc9dac0436e0555c01
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-09-08 14:49:52 +0900

    Title: 숫자의 순차적 이동, Time: 329ms, Memory: 18MB, Status: Passed - Codetree

--- 250908/숫자의 순차적 이동/sequential-movement-of-numbers.java
+++ 250908/숫자의 순차적 이동/sequential-movement-of-numbers.java
@@ -9,6 +9,7 @@ import java.util.*;
 public class Main {
     static int n;
     static int[][] arr;
+    static List<Pair> pairs = new ArrayList<>();
 
 
     static void go(int x, int y) {
@@ -29,37 +30,74 @@ public class Main {
             int ny = y + dy[k];
             if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
             if (arr[nx][ny] == max) {
+
+                int curr = arr[x][y];
+                int next = arr[nx][ny];
+                pairs.set(curr - 1, new Pair(nx, ny, max));
+                pairs.set(next - 1, new Pair(x, y, arr[x][y]));
+
+
                 int temp = arr[nx][ny];
                 arr[nx][ny] = arr[x][y];
                 arr[x][y] = temp;
+
+
                 return;
             }
 
         }
     }
 
+    static class Pair {
+        int x;
+        int y;
+        int num;
+
+        Pair(int x, int y, int num) {
+            this.x = x;
+            this.y = y;
+            this.num = num;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         arr = new int[n][n];
+
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
+
             }
         }
+
 
         while (m-- > 0) {
 
 
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    go(i, j);
+                    pairs.add(new Pair(i, j, arr[i][j]));
                 }
             }
+
+
+            pairs.sort(
+                    Comparator.comparing(
+                            (Pair p) -> p.num
+                    )
+            );
+
+
+            for (Pair pair : pairs) {
+                go(pair.x, pair.y);
+            }
+            pairs.clear();
 
 
         }
