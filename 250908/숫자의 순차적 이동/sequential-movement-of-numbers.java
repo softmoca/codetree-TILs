d2ec1f39b1b0c126ceb6f206f1a27cc41b1271c0
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-09-08 14:50:16 +0900

    Title: 숫자의 순차적 이동, Time: 202ms, Memory: 14MB, Status: Passed - Codetree

--- 250908/숫자의 순차적 이동/sequential-movement-of-numbers.java
+++ 250908/숫자의 순차적 이동/sequential-movement-of-numbers.java
@@ -1,114 +1,95 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
-/*
-
- */
-
+class Pair { 
+    int x, y;
+    public Pair(int x, int y) { 
+        this.x = x; 
+        this.y = y; 
+    } 
+}
 
 public class Main {
-    static int n;
-    static int[][] arr;
-    static List<Pair> pairs = new ArrayList<>();
-
-
-    static void go(int x, int y) {
-        int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
-        int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
-
-        int max = 0;
-        for (int k = 0; k < 8; k++) {
-            int nx = x + dx[k];
-            int ny = y + dy[k];
-            if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
-            max = Math.max(max, arr[nx][ny]);
-        }
-
-
-        for (int k = 0; k < 8; k++) {
-            int nx = x + dx[k];
-            int ny = y + dy[k];
-            if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
-            if (arr[nx][ny] == max) {
-
-                int curr = arr[x][y];
-                int next = arr[nx][ny];
-                pairs.set(curr - 1, new Pair(nx, ny, max));
-                pairs.set(next - 1, new Pair(x, y, arr[x][y]));
-
-
-                int temp = arr[nx][ny];
-                arr[nx][ny] = arr[x][y];
-                arr[x][y] = temp;
-
-
-                return;
+    public static final int DIR_NUM = 8;
+    public static final int MAX_N = 20;
+    
+    public static int n, m;
+    public static int[][] grid = new int[MAX_N][MAX_N];
+    
+    public static boolean inRange(int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < n;
+    }
+    
+    public static Pair FindPos(int num) {
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                if(grid[i][j] == num)
+                    return new Pair(i, j);
+        
+        return new Pair(0, 0);
+    }
+    
+    // 그 다음 위치를 찾아 반환합니다.
+    public static Pair NextPos(Pair pos) {
+        int[] dx = new int[]{-1, -1, -1,  0, 0,  1, 1, 1};
+        int[] dy = new int[]{ -1, 0,  1, -1, 1, -1, 0, 1};
+        
+        int x = pos.x;
+        int y = pos.y;
+        
+        // 인접한 8개의 칸 중 가장 값이 큰 위치를 찾아 반환합니다.
+        int maxVal = -1;
+        Pair maxPos = new Pair(0, 0);
+        for(int i = 0; i < 8; i++) {
+            int nx = x + dx[i], ny = y + dy[i];
+            if(inRange(nx, ny) && grid[nx][ny] > maxVal) {
+                maxVal = grid[nx][ny];
+                maxPos = new Pair(nx, ny);
             }
-
+        }
+        
+        return maxPos;
+    }
+    
+    public static void swap(Pair pos, Pair nextPos) {
+        int x = pos.x;
+        int y = pos.y;
+        
+        int nx = nextPos.x;
+        int ny = nextPos.y;
+    
+        int temp = grid[x][y];
+        grid[x][y] = grid[nx][ny];
+        grid[nx][ny] = temp;
+    }
+    
+    public static void simulate() {
+        // 번호가 증가하는 순으로
+        // 그 다음 위치를 구해
+        // 한 칸씩 움직입니다.
+        for(int num = 1; num <= n * n; num++) {
+            Pair pos = FindPos(num);
+            Pair nextPos = NextPos(pos);
+            swap(pos, nextPos);
         }
     }
 
-    static class Pair {
-        int x;
-        int y;
-        int num;
-
-        Pair(int x, int y, int num) {
-            this.x = x;
-            this.y = y;
-            this.num = num;
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        arr = new int[n][n];
-
-
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-
-            }
-        }
-
-
-        while (m-- > 0) {
-
-
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    pairs.add(new Pair(i, j, arr[i][j]));
-                }
-            }
-
-
-            pairs.sort(
-                    Comparator.comparing(
-                            (Pair p) -> p.num
-                    )
-            );
-
-
-            for (Pair pair : pairs) {
-                go(pair.x, pair.y);
-            }
-            pairs.clear();
-
-
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(arr[i][j] + " ");
-            }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+            
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                grid[i][j] = sc.nextInt();
+        
+        // m번 시뮬레이션을 진행합니다.
+        while(m-- > 0)
+            simulate();
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++)
+                System.out.print(grid[i][j] + " ");
             System.out.println();
         }
-
-
     }
 }
