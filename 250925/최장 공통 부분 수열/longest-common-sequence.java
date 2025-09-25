900c6dca4aa875252d7fa607a4fa9a4e1439820b
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-09-25 14:45:48 +0900

    Title: 최장 공통 부분 수열, Time: 134ms, Memory: 13MB, Status: Passed - Codetree

---
+++ 250925/최장 공통 부분 수열/longest-common-sequence.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+/*
+
+ */
+
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+
+        str1 = " " + str1;
+        str2 = " " + str2;
+
+
+        int[][] dp = new int[str1.length()][str2.length()];
+
+        for (int i = 1; i < str1.length(); i++) {
+            for (int j = 1; j < str2.length(); j++) {
+                if (str1.charAt(i) == str2.charAt(j)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+        System.out.println(dp[str1.length() - 1][str2.length() - 1]);
+
+
+    }
+}
