bf4c533cbecb808ee0b03656603a638b15cc4660
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-10-22 11:24:19 +0900

    Title: 범위 내에 있는 점의 수 2, Time: 1675ms, Memory: 59MB, Status: Runtime Error - Codetree

---
+++ 251022/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int q = sc.nextInt();
+
+        int[] arr= new int[1000001];
+        int[] sum= new int[1000001];
+
+
+
+
+        for (int i = 0; i < n; i++) {
+            int x= sc.nextInt();
+            arr[x]=1;
+        }
+        for(int i=1;i<=1000000;i++){
+            sum[i]=sum[i-1]+arr[i];
+        }
+
+
+
+
+        for (int i = 0; i < q; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(sum[b]-sum[a-1]);
+
+        }
+
+
+
+
+
+
+
+    }
+}
