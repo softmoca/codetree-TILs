4d26c88e5fcc3a341a14b32d980bbb7da9c4ee25
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-10-22 10:52:19 +0900

    Title: 부분 수열의 합이 K, Time: 195ms, Memory: 12MB, Status: Passed - Codetree

--- 251022/부분 수열의 합이 K/the-sum-of-the-subsequences-is-k.java
+++ 251022/부분 수열의 합이 K/the-sum-of-the-subsequences-is-k.java
@@ -5,13 +5,37 @@ public class Main {
         int n = sc.nextInt();
         int k = sc.nextInt();
         int[] arr = new int[n+1];
+        int[] sum = new int[n+1];
+
         for (int i = 1; i <= n; i++) {
             arr[i] = sc.nextInt();
         }
 
+        sum[0]=0;
+        for(int i=1; i<=n;i++){
+            sum[i]=sum[i-1]+arr[i];
+        }
+        int cnt=0;
 
-
+        for(int size=1; size<=n;size++){
+            cnt=cnt+find(size,sum,n,k);
+        }
+        System.out.print(cnt);
 
 
     }
+    static int find(int size,int[] sum,int n,int k){
+        int tempCnt=0;
+        for(int i=size;i<=n;i++){
+            if(sum[i]-sum[i-size]==k){
+tempCnt++;
+            }
+        }
+
+return tempCnt;
+
+    }
+
+
+
 }
