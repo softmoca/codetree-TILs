fb2497a0ccddd0ca5b691857a847f21e6f5377cd
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-10-23 11:11:06 +0900

    Title: 미래가 보이는 가위바위보, Time: 130ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251023/미래가 보이는 가위바위보/rock-paper-scissors-to-see-the-future.java
@@ -0,0 +1,109 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        char[] matches = new char[n + 1];
+        for (int i = 1; i <= n; i++) {
+            char x = sc.next().charAt(0);
+            if (x == 'P') {
+                x = 'p';
+            } else if (x == 'H') {
+                x = 'h';
+            } else {
+                x = 's';
+            }
+
+            matches[i] = x;
+        }
+
+        int[] hl = new int[n + 2];
+        int[] hr = new int[n + 2];
+        int[] sl = new int[n + 2];
+        int[] sr = new int[n + 2];
+        int[] pl = new int[n + 2];
+        int[] pr = new int[n + 2];
+
+        for (int i = 1; i <= n; i++) {// 주먹 l
+            if (matches[i] == 's') {
+                hl[i] = hl[i - 1] + 1;
+            } else {
+                hl[i] = hl[i - 1];
+            }
+        }
+
+        for (int i = n; i > 0; i--) { // 주먹 r
+            if (matches[i] == 's') {
+                hr[i] = hr[i + 1] + 1;
+            } else {
+                hr[i] = hr[i + 1];
+            }
+        }
+
+
+        for (int i = 1; i <= n; i++) {// 가위 l
+            if (matches[i] == 'p') {
+                sl[i] = sl[i - 1] + 1;
+            } else {
+                sl[i] = sl[i - 1];
+            }
+        }
+
+
+        for (int i = n; i > 0; i--) { // 가위 r
+            if (matches[i] == 'p') {
+                sr[i] = sr[i + 1] + 1;
+            } else {
+                sr[i] = sr[i + 1];
+            }
+        }
+
+
+        for (int i = 1; i <= n; i++) {// 보자기 l
+            if (matches[i] == 'h') {
+                pl[i] = pl[i - 1] + 1;
+            } else {
+                pl[i] = pl[i - 1];
+            }
+        }
+
+
+        for (int i = n; i > 0; i--) { // 보자기 r
+            if (matches[i] == 'h') {
+                pr[i] = pr[i + 1] + 1;
+            } else {
+                pr[i] = pr[i + 1];
+            }
+        }
+
+
+        int res = 0;
+        for (int i = 1; i < n; i++) { // 주 -> 가
+            res = Math.max(hl[i] + sr[i + 1], res);
+        }
+
+        for (int i = 1; i < n; i++) { // 주 -> 보
+            res = Math.max(hl[i] + pr[i + 1], res);
+        }
+
+        for (int i = 1; i < n; i++) {// 가 -> 주
+            res = Math.max(sl[i] + hr[i + 1], res);
+        }
+
+        for (int i = 1; i < n; i++) { // 가 -> 보
+            res = Math.max(sl[i] + pr[i + 1], res);
+        }
+
+        for (int i = 1; i < n; i++) { // 보 -> 바
+            res = Math.max(pl[i] + hr[i + 1], res);
+        }
+
+        for (int i = 1; i < n; i++) { // 보 -> 가
+            res = Math.max(pl[i] + sr[i + 1], res);
+        }
+        System.out.println(res);
+
+        // Please write your code here.
+    }
+}
