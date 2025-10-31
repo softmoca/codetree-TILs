72d2323e1c4019bfb02a8e8d97513eb2611ffc58
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-10-31 15:25:18 +0900

    Title: 연속하는 정수 N개의 합, Time: 3018ms, Memory: 10MB, Status: Time Limit Exceed - Codetree

---
+++ 251031/연속하는 정수 N개의 합/sum-of-consecutive-n-integers.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n+1];
+        for (int i = 1; i <=n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        int p1=0;
+        int p2=1;
+        int sum=arr[1];
+        int cnt=0;
+        while(true){
+            if( sum==m){
+                cnt++;
+            }else if(sum<m){
+                if(p2==n) break;
+                sum+=arr[++p2];
+            }else if(sum>m){
+                if(p1==n) break;
+                sum-=arr[p1++];
+            }
+        }
+        System.out.println(cnt);
+        
+        
+        
+        
+        
+        
+        
+    }
+}
