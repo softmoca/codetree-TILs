a7a263f2cba9b0bcabb48a998970f58c195c0be0
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-11-05 15:44:15 +0900

    Title: 삼 오 무, Time: 86ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 251105/삼 오 무/three-five-moo.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+/*
+
+ */
+
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int l = 1;
+        int r = 1_000_000_000;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            int target = mid - mid / 3 - mid / 5 + mid / 15;
+
+            if (target < n) {
+                l = mid + 1;
+            } else if (target > n) {
+                r = mid - 1;
+            } else {
+                System.out.println(mid);
+                break;
+            }
+
+
+        }
+
+
+    }
+}
