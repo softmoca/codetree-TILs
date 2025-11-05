a6ff6c54b74de9daff51f8587008af995716c08b
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-11-05 12:31:53 +0900

    Title: 겹치는 원소가 적은 부분 수열, Time: 333ms, Memory: 21MB, Status: Passed - Codetree

--- 251105/겹치는 원소가 적은 부분 수열/subsequences-with-few-overlapping-elements.java
+++ 251105/겹치는 원소가 적은 부분 수열/subsequences-with-few-overlapping-elements.java
@@ -1,5 +1,5 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -12,19 +12,33 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) a[i] = Integer.parseInt(st.nextToken());
 
-        Map<Integer, Integer> freq = new HashMap<>();
+        // 1) 좌표 압축 준비
+        int[] compSrc = a.clone();
+        Arrays.sort(compSrc);
+        int m = 0;
+        for (int i = 0; i < N; i++) {
+            if (i == 0 || compSrc[i] != compSrc[i-1]) compSrc[m++] = compSrc[i];
+        }
+        // compSrc[0..m-1]에 고유값 정렬 저장
+
+        // 2) a를 압축 인덱스로 변환
+        int[] idx = new int[N];
+        for (int i = 0; i < N; i++) {
+            idx[i] = Arrays.binarySearch(compSrc, 0, m, a[i]); // 0..m-1 반환
+        }
+
+        // 3) 배열로 빈도 관리 + 슬라이딩 윈도우
+        int[] freq = new int[m];
         int ans = 0, l = 0;
-
         for (int r = 0; r < N; r++) {
-            // 오른쪽 값 추가
-            freq.put(a[r], freq.getOrDefault(a[r], 0) + 1);
+            int vr = idx[r];
+            freq[vr]++;
 
-            // 어떤 값이 K를 초과하면 왼쪽을 줄여서 조건 회복
-            while (freq.get(a[r]) > K) {
-                freq.put(a[l], freq.get(a[l]) - 1);
+            while (freq[vr] > K) {           // 조건 위반 시 왼쪽 축소
+                int vl = idx[l];
+                freq[vl]--;
                 l++;
             }
-
             ans = Math.max(ans, r - l + 1);
         }
 
