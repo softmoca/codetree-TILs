25d822d592c22fde9502d42f3ed5ea1c35e19e1a
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-11-06 17:26:54 +0900

    Title: 각 정점까지의 최단 경로 3, Time: 88ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 251106/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.*;
+
+/*
+
+ */
+
+
+public class Main {
+    static int n, m;
+    static List<int[]>[] graph;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        graph = new List[n + 1];
+
+        for (int i = 0; i < n + 1; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            graph[start].add(new int[]{end, v});
+        }
+
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[1] = 0;
+
+
+        Queue<Integer> pq = new PriorityQueue<>();
+        pq.offer(1);
+
+        while (!pq.isEmpty()) {
+
+
+            int target = pq.poll();
+            int targetDist = dist[target];
+
+            for (int[] next : graph[target]) {
+                int node = next[0];
+                int val = next[1];
+
+                if (targetDist + val < dist[node]) {
+                    dist[node] = targetDist + val;
+                    pq.offer(node);
+                }
+
+            }
+
+
+        }
+
+        for (int i = 2; i <= n; i++) {
+            if (dist[i] == Integer.MAX_VALUE) System.out.println("INF");
+            else System.out.println(dist[i]);
+        }
+
+
+    }
+}
