a33910542dd9a2100aa399067fe3138fae44dc65
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-11-28 14:14:15 +0900

    Title: 트리 정점 거리, Time: 504ms, Memory: 26MB, Status: Passed - Codetree

--- 251128/트리 정점 거리/node-distance.java
+++ 251128/트리 정점 거리/node-distance.java
@@ -1,69 +1,75 @@
-import java.io.*;
-import java.lang.reflect.Array;
-import java.util.*;
+import java.util.Scanner;
+import java.util.ArrayList;
 
-/*
-방법 1 그냥 dfs
-방법 2 루트로 부터 거리 구하기
- */
+class Pair {
+    int y, d;
 
+    public Pair(int y, int d) {
+        this.y = y;
+        this.d = d;
+    }
+}
 
 public class Main {
+    public static final int MAX_N = 1000;
+    
+    // 변수 선언:
+    public static int n, m;
+    public static ArrayList<Pair>[] edges = new ArrayList[MAX_N + 1];
+    public static boolean[] visited = new boolean[MAX_N + 1];
+    public static int[][] dist = new int[MAX_N + 1][MAX_N + 1];
+    
+    // DFS를 통해 st로부터 모든 정점까지의 거리를 탐색합니다.
+    public static void dfs(int st, int x) {
+        for(int i = 0; i < (int) edges[x].size(); i++) {
+            int y = edges[x].get(i).y;
+            int d = edges[x].get(i).d;
 
-    static List<int[]>[] graph;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        graph = new List[n + 1];
-        dist = new int[n + 1];
-        for (int i = 0; i <= n; i++) {
-            graph[i] = new ArrayList<int[]>();
+            // 이미 방문한 노드는 스킵합니다.
+            if(visited[y]) continue;
+            
+            visited[y] = true;
+    
+            // st로부터의 거리를 갱신합니다.
+            dist[st][y] = dist[st][x] + d;
+            dfs(st, y);
         }
-
-        for (int i = 0; i < n - 1; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-            graph[a].add(new int[]{b, cost});
-            graph[b].add(new int[]{a, cost});
-
-        }
-
-        for (int i = 0; i < m; i++) {
-
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            dist = new int[n + 1];
-            Arrays.fill(dist, -1);
-            dist[a] = 0;
-            dfs(a, b);
-            System.out.println(dist[b]);
-
-
-        }
-
     }
 
-    static int[] dist;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
+        m = sc.nextInt();
 
-    static void dfs(int start, int target) {
+        for(int i = 1; i <= n; i++)
+            edges[i] = new ArrayList<>();
+        
+        // n - 1개의 간선 정보를 입력받습니다.
+        for(int i = 1; i <= n - 1; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int d = sc.nextInt();
 
-        for (int[] now : graph[start]) {
-            int node = now[0];
-            int cost = now[1];
-            if (dist[node] == -1) {
-                dist[node] = dist[start] + cost;
-                dfs(node, target);
-            }
+            // 간선 정보를 인접리스트에 넣어줍니다.
+            edges[x].add(new Pair(y, d));
+            edges[y].add(new Pair(x, d));
         }
 
+        // 각 n개의 정점에 대해, 모든 노드간의 거리를 DFS로 갱신해줍니다.
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) 
+                visited[j] = false;
+            
+            visited[i] = true;
+            dfs(i, i);
+        }
+
+        // m개의 노드 쌍을 입력받고, 두 노드 쌍 간의 거리를 바로 출력해줍니다.
+        for(int i = 1; i <= m; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            System.out.println(dist[x][y]);
+        }
     }
-
-
 }
