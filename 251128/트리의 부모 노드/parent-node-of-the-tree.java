96f2e4a419571995b1827fd6ea9a1a431f6699a7
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-11-28 12:40:28 +0900

    Title: 트리의 부모 노드, Time: 145ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251128/트리의 부모 노드/parent-node-of-the-tree.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+/*
+
+ */
+
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n + 1];
+
+        for (int i = 1; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            arr[b] = a;
+
+        }
+
+        for (int i = 2; i <= n; i++) {
+            System.out.println(arr[i]);
+        }
+
+
+    }
+}
