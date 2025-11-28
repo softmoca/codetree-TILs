b84be2c22d6fa46c594f0134635908b0cc3a50bd
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-11-28 15:22:39 +0900

    Title: 트리 수정, Time: 864ms, Memory: 26MB, Status: Passed - Codetree

--- 251128/트리 수정/modify-tree.java
+++ 251128/트리 수정/modify-tree.java
@@ -1,144 +1,116 @@
-import java.io.*;
-import java.lang.reflect.Array;
-import java.util.*;
+import java.util.Scanner;
+import java.util.ArrayList;
 
-/*
+class Pair {
+    int y, d;
 
- */
-
+    public Pair(int y, int d) {
+        this.y = y;
+        this.d = d;
+    }
+}
 
 public class Main {
-    static int n;
-    static List<int[]>[] graph;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-
-        int[][] arr = new int[n - 1][3];
-        for (int i = 0; i < n - 1; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
-            arr[i][2] = Integer.parseInt(st.nextToken());
-        }
-
-        int res = -1;
-        for (int i = 0; i < n - 1; i++) {
-
-            graph = new List[n];
-            for (int j = 0; j < n; j++) {
-                graph[j] = new ArrayList<>();
+    public static final int MAX_N = 2000;
+    
+    // 변수 선언:
+    public static int n;
+    public static ArrayList<Pair>[] edge = new ArrayList[MAX_N];
+    public static boolean[][] removed = new boolean[MAX_N][MAX_N];
+    public static int[] edgeX = new int[MAX_N];
+    public static int[] edgeY = new int[MAX_N];
+    public static int[] edgeDist = new int[MAX_N];
+    
+    public static boolean[] visited = new boolean[MAX_N];
+    public static int[] dist = new int[MAX_N];
+    
+    public static int maxDist, lastNode;
+    public static int ans;
+    
+    // 모든 노드의 정점을 탐색하는 DFS를 진행합니다.
+    public static void dfs(int x) {
+        for(int i = 0; i < edge[x].size(); i++) {
+            int y = edge[x].get(i).y;
+            int d = edge[x].get(i).d;
+    
+            // 지워진 간선이면 스킵합니다.
+            if(removed[x][y]) continue;
+    
+            // 이미 방문한 정점이면 스킵합니다.
+            if(visited[y]) continue;
+    
+            visited[y] = true;
+            dist[y] = dist[x] + d;
+    
+            // 현재 정점을 기준으로 가장 먼 노드를 찾습니다.
+            if(dist[y] > maxDist) {
+                maxDist = dist[y];
+                lastNode = y;
             }
-
-
-            int cost = arr[i][2];
-
-            for (int j = 0; j < n - 1; j++) {
-                if (j == i) continue;
-                int a = arr[j][0];
-                int b = arr[j][1];
-                int c = arr[j][2];
-
-
-                graph[a].add(new int[]{b, c});
-                graph[b].add(new int[]{a, c});
-
-
-            }
-            dist = new int[n];
-            Arrays.fill(dist, -1);
-            dist[0] = 0;
-
-            dfs(0);
-
-            int maxNode = -1;
-            int maxValue = -1;
-            for (int k = 0; k < n; k++) {
-                if (maxValue < dist[k]) {
-                    maxValue = dist[k];
-                    maxNode = k;
-                }
-            }
-
-            Arrays.fill(dist, -1);
-
-            dist[maxNode] = 0;
-            dfs(maxNode);
-
-
-            maxNode = -1;
-            maxValue = -1;
-            for (int k = 0; k < n; k++) {
-                if (maxValue < dist[k]) {
-                    maxValue = dist[k];
-                    maxNode = k;
-                }
-            }
-
-            int maxxx1 = maxValue;
-
-            int temp = -1;
-            for (int j = 0; j < n; j++) {
-                if (dist[j] == -1) {
-                    temp = j;
-                    break;
-                }
-            }
-
-            //
-            Arrays.fill(dist, -1);
-            dist[temp] = 0;
-            dfs(temp);
-
-            maxNode = -1;
-            maxValue = -1;
-            for (int k = 0; k < n; k++) {
-                if (maxValue < dist[k]) {
-                    maxValue = dist[k];
-                    maxNode = k;
-                }
-            }
-
-            Arrays.fill(dist, -1);
-
-            dist[maxNode] = 0;
-            dfs(maxNode);
-
-
-            maxNode = -1;
-            maxValue = -1;
-            for (int k = 0; k < n; k++) {
-                if (maxValue < dist[k]) {
-                    maxValue = dist[k];
-                    maxNode = k;
-                }
-            }
-
-            int maxxx2 = maxValue;
-            res = Math.max(res, maxxx1 + maxxx2 + cost);
-
-        }
-        System.out.println(res);
-
-
-    }
-
-    static int[] dist;
-
-
-    static void dfs(int start) {
-
-        for (int[] now : graph[start]) {
-            int nextNode = now[0];
-            int cost = now[1];
-
-            if (dist[nextNode] == -1) {
-                dist[nextNode] = dist[start] + cost;
-                dfs(nextNode);
-            }
+    
+            dfs(y);
         }
     }
+    
+    public static int getTreeDiameter(int x) {
+        // 전역변수들을 초기화합니다.
+        for(int i = 0; i < n; i++) visited[i] = false;
+        for(int i = 0; i < n; i++) dist[i] = 0;
+        maxDist = 0;
+        lastNode = x;
+    
+        // dfs를 통해 가장 멀리 있는 정점을 찾습니다.
+        visited[x] = true;
+        dfs(x);
+    
+        // 전역변수들을 초기화합니다.
+        for(int i = 0; i < n; i++) visited[i] = false;
+        for(int i = 0; i < n; i++) dist[i] = 0;
+        maxDist = 0;
+    
+        // dfs를 통해 가장 멀리 있는 정점에서 트리의 지름을 찾습니다. 
+        visited[lastNode] = true;   
+        dfs(lastNode);
+    
+        return maxDist;
+    }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
 
+        for(int i = 0; i < n; i++)
+            edge[i] = new ArrayList<>();
+
+        // n - 1개의 간선 정보를 입력받습니다.
+        for(int i = 0; i < n - 1; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int d = sc.nextInt();
+
+            edgeX[i] = x;
+            edgeY[i] = y;
+            edgeDist[i] = d;
+            edge[x].add(new Pair(y, d));
+            edge[y].add(new Pair(x, d));
+        }
+
+        // n - 1개의 간선을 하나씩 지우고 생긴 두 트리 각각의 지름을 찾습니다.
+        for(int i = 0; i < n - 1; i++) {
+            removed[edgeX[i]][edgeY[i]] = true;
+            removed[edgeY[i]][edgeX[i]] = true;
+
+            // i번 간선을 지우고 새로 그었을 때, 만들어질 수 있는 최대 지름을 찾습니다.
+            // i번 간선을 지운 뒤 생긴 두 트리에서의 지름을 새로 연결할 때 최대 지름이 됩니다.
+            int maxDiameter = edgeDist[i] + getTreeDiameter(edgeX[i]) + getTreeDiameter(edgeY[i]);
+            ans = Math.max(ans, maxDiameter);
+
+            removed[edgeX[i]][edgeY[i]] = false;
+            removed[edgeY[i]][edgeX[i]] = false;
+        }
+
+        // 만들 수 있는 트리의 가장 큰 지름을 출력합니다.
+        System.out.print(ans);
+    }
 }
