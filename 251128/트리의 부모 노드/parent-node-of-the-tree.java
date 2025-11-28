ce2cfbc6a57afcc9aae32b69f9d9f52b6d5213cc
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-11-28 12:46:53 +0900

    Title: 트리의 부모 노드, Time: 1906ms, Memory: 68MB, Status: Passed - Codetree

--- 251128/트리의 부모 노드/parent-node-of-the-tree.java
+++ 251128/트리의 부모 노드/parent-node-of-the-tree.java
@@ -1,33 +1,56 @@
-import java.io.*;
-import java.util.*;
-
-/*
-
- */
-
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
+    public static final int MAX_N = 100000;
+
+    // 변수 선언:
+    public static int n;
+    public static ArrayList<Integer>[] edges = new ArrayList[MAX_N + 1];
+    public static boolean[] visited = new boolean[MAX_N + 1];
+    public static int[] parent = new int[MAX_N + 1];
+
+    // 트리 순회를 진행합니다.
+    // DFS 방식으로 진행하며,
+    // 진행되는 간선에 대해 (부모, 자식) 관계를 정의해줍니다.
+    public static void traversal(int x) {
 
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        int[] arr = new int[n + 1];
-
-        for (int i = 1; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-
-            arr[b] = a;
-
+        for (int node : edges[x]) {
+            if (!visited[node]) {
+                visited[node] = true;
+                parent[node] = x;
+                traversal(node);
+            }
         }
 
-        for (int i = 2; i <= n; i++) {
-            System.out.println(arr[i]);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++)
+            edges[i] = new ArrayList<>();
+
+        // n - 1개의 간선 정보를 입력받습니다.
+        for (int i = 1; i <= n - 1; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            // 간선 정보를 인접리스트에 넣어줍니다.
+            edges[x].add(y);
+            edges[y].add(x);
         }
 
+        // 1번부터 트리 순회를 진행합니다.
+        visited[1] = true;
+        traversal(1);
 
+        // 부모 노드를 출력합니다.
+        for (int i = 2; i <= n; i++)
+            System.out.println(parent[i]);
     }
 }
