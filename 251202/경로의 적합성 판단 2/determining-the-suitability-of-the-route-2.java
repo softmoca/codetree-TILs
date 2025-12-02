eb205a6ce1cdb4775c10af721d6f41282d4efe88
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-12-02 14:36:08 +0900

    Title: 경로의 적합성 판단 2, Time: 347ms, Memory: 22MB, Status: Passed - Codetree

--- 251202/경로의 적합성 판단 2/determining-the-suitability-of-the-route-2.java
+++ 251202/경로의 적합성 판단 2/determining-the-suitability-of-the-route-2.java
@@ -7,9 +7,18 @@ import java.util.*;
 
 
 public class Main {
+    static int[] parent;
 
-    static List<Integer>[] graph;
-    static int[] arr;
+    static int find(int x) {
+        if (x == parent[x]) {
+            return x;
+        }
+        return parent[x] = find(parent[x]);
+    }
+
+    static void union(int a, int b) {
+        parent[find(a)] = find(b);
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,61 +28,40 @@ public class Main {
         int m = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        graph = new List[n + 1];
-        arr = new int[k];
-
+        parent = new int[n + 1];
         for (int i = 0; i < n + 1; i++) {
-            graph[i] = new ArrayList<>();
+            parent[i] = i;
         }
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            graph[a].add(b);
-            graph[b].add(a);
-
+            union(a, b);
         }
+        // System.out.println("d");
 
-
+        int[] arr = new int[k + 1];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < k; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        check = new boolean[n + 1];
 
 
         for (int i = 0; i < k - 1; i++) {
-            int start = arr[i];
-            int end = arr[i + 1];
-            Arrays.fill(check, false);
-            check[start] = true;
-            dfs(start);
-            if (!check[end]) {
+            int a = arr[i];
+            int b = arr[i + 1];
+            if (find(a) != find(b)) {
                 System.out.println(0);
                 System.exit(0);
+
             }
 
-
         }
+
+
         System.out.println(1);
 
-    }
-
-    static boolean[] check;
-
-    static void dfs(int startNode) {
-
-        for (int next : graph[startNode]) {
-
-            if (!check[next]) {
-                check[next] = true;
-                dfs(next);
-            }
-        }
-
 
     }
-
-
 }
