ed0b0defe3a600849ae0729bd08f64af22cc0a4d
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-12-02 14:16:16 +0900

    Title: 경로의 적합성 판단 2, Time: 713ms, Memory: 176MB, Status: Memory Limit Exceed - Codetree

---
+++ 251202/경로의 적합성 판단 2/determining-the-suitability-of-the-route-2.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.util.*;
+
+/*
+
+ */
+
+
+public class Main {
+
+    static List<Integer>[] graph;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        graph = new List[n + 1];
+        arr = new int[k];
+
+        for (int i = 0; i < n + 1; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a].add(b);
+            graph[b].add(a);
+
+        }
+
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < k; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+
+        for (int i = 0; i < k - 1; i++) {
+            int start = arr[i];
+            int end = arr[i + 1];
+            check = new boolean[n + 1];
+            check[start] = true;
+            dfs(start);
+            if (!check[end]) {
+                System.out.println(0);
+                System.exit(0);
+            }
+
+
+        }
+        System.out.println(1);
+
+    }
+
+    static boolean[] check;
+
+    static void dfs(int startNode) {
+
+        for (int next : graph[startNode]) {
+
+            if (!check[next]) {
+                check[next] = true;
+                dfs(next);
+            }
+        }
+
+
+    }
+
+
+}
