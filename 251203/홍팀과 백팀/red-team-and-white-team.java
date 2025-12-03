03be3b79365c8745abb1665e2f43e1a4ca659974
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-12-03 15:32:18 +0900

    Title: 홍팀과 백팀, Time: 238ms, Memory: 18MB, Status: Passed - Codetree

--- 251203/홍팀과 백팀/red-team-and-white-team.java
+++ 251203/홍팀과 백팀/red-team-and-white-team.java
@@ -1,13 +1,17 @@
 import java.io.*;
 import java.util.*;
 
-/*
-
- */
-
-
 public class Main {
+    static int[] parent;
 
+    static int find(int x) {
+        if (x == parent[x]) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    static void union(int a, int b) {
+        parent[find(a)] = find(b);
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,44 +20,30 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-
-        int[] arr = new int[n + 1];
-
+        // i = 홍팀 버전, i+n = 백팀 버전
+        parent = new int[2 * n + 1];
+        for (int i = 1; i <= 2 * n; i++) {
+            parent[i] = i;
+        }
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            int aa = arr[a];
-            int bb = arr[b];
+            // a와 b는 다른 팀이어야 함
+            // → a의 홍팀 = b의 백팀
+            // → a의 백팀 = b의 홍팀
+            union(a, b + n);
+            union(a + n, b);
 
-
-            if (aa == 0 && bb == 0) {
-                arr[a] = -1;
-                arr[b] = 1;
-
-            } else if (aa == 1 && bb == 0) {
-                arr[b] = -1;
-
-            } else if (aa == 0 && bb == 1) {
-                arr[a] = -1;
-
-            } else if (aa == -1 && bb == 0) {
-                arr[b] = 1;
-            } else if (aa == 0 && bb == -1) {
-                arr[a] = 1;
-            } else if (aa == 1 && bb == 1) {
+            // a의 홍팀 버전과 백팀 버전이 같은 그룹이면 모순!
+            if (find(a) == find(a + n) || find(b) == find(b + n)) {
                 System.out.println(0);
-                System.exit(0);
-            } else if (aa == -1 && bb == -1) {
-                System.out.println(0);
-                System.exit(0);
+                return;
             }
-
-
         }
-        System.out.println(1);
 
+        System.out.println(1);
     }
 }
