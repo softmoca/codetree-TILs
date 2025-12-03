8257777850c24f0cd3cb83597acd1e940fc95cfe
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-12-03 15:08:51 +0900

    Title: 칸에 입력되는 정수, Time: 787ms, Memory: 35MB, Status: Passed - Codetree

--- 251203/칸에 입력되는 정수/integer-in-the-cell.java
+++ 251203/칸에 입력되는 정수/integer-in-the-cell.java
@@ -1,42 +1,56 @@
-import java.io.*;
-import java.util.*;
-
-/*
-
- */
-
+import java.util.Scanner;
 
 public class Main {
+    public static final int MAX_N = 100000;
+    
+    // 변수 선언
+    public static int n, m;
+    
+    // 번호별 그룹을 관리합니다.
+    // uf[i] : i번 보다 작거나 같은 위치 중에 값이 들어갈 수 있는 최대 위치를 의미합니다.
+    public static int[] uf = new int[MAX_N + 1];
+    
+    // x의 대표 번호를 찾아줍니다.
+    public static int find(int x) {
+        // x가 루트 노드라면 x값을 반환합니다.
+        if(uf[x] == x)
+            return x;
+        // x가 루트 노드가 아니라면
+        // x의 부모인 uf[x]에서 탐색을 더 진행한 뒤
+        // 찾아낸 루트 노드를 uf[x]에 넣어줌과 동시에
+        // 해당 노드값을 반환합니다.
+        return uf[x] = find(uf[x]);
+    }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 입력:
+        n = sc.nextInt();
+        m = sc.nextInt();
 
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
+        // 초기 uf 값을 설정합니다.
+        for(int i = 1; i <= n; i++)
+            uf[i] = i;
 
-        int[] arr = new int[n + 1];
-        int res = 0;
-        for (int i = 0; i < m; i++) {
-            int x = Integer.parseInt(br.readLine());
+        // m개의 정보를 입력받습니다. x번 보다 작거나 같은 위치에 값이 들어갈 수 있는지 확인합니다.
+        int ans = m;
+        for(int i = 1; i <= m; i++) {
+            int x = sc.nextInt();
 
-            boolean flag = false;
-
-            for (int j = x; j >= 1; j--) {
-                if (arr[j] == 0) {
-                    arr[j] = x;
-                    flag = true;
-                    break;
-                }
+            // x번보다 작거나 같은 위치 중에 값이 들어갈 수 있는 최대 위치로 이동합니다.
+            int X = find(x);
+            if(X == 0) {
+                // 그런 값이 없다면 공을 넣을 수 없습니다.
+                ans = i - 1;
+                break;
             }
 
-            if (!flag) break;
-            res++;
-
-
+            // X번보다 작은 위치 중에 값이 들어갈 수 있는 최대 위치로 이동합니다.
+            int Y = find(X - 1);
+            uf[X] = Y;
         }
 
-        System.out.println(res);
-
+        System.out.print(ans);
     }
 }
