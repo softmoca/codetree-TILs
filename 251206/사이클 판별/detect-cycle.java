1f3064ac5d0be925bd69df5726e92b06d30a46c0
YoungChul Choi <105472863+softmoca@users.noreply.github.com>
2025-12-06 18:56:37 +0900

    Title: 사이클 판별, Time: 472ms, Memory: 27MB, Status: Passed - Codetree

---
+++ 251206/사이클 판별/detect-cycle.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.util.*;
+
+/*
+
+ */
+
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        int n=Integer.parseInt(st.nextToken());
+        int m=Integer.parseInt(st.nextToken());
+
+        int[] inDegree = new int[n + 1];
+
+        List<Integer>[] graph = new List[n + 1];
+
+        for (int i = 0; i < n + 1; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a=Integer.parseInt(st.nextToken());
+            int b=Integer.parseInt(st.nextToken());
+
+            inDegree[b]++;
+            graph[a].add(b);
+        }
+
+
+
+        Queue<Integer> qu = new ArrayDeque<>();
+
+
+        for (int i = 1; i < n+1; i++) {
+            if (inDegree[i] == 0) {
+                qu.offer(i);
+            }
+        }
+
+
+        List<Integer> res = new ArrayList<>();
+
+        while (!qu.isEmpty()) {
+
+            int curr = qu.poll();
+            res.add(curr);
+
+            for (int next : graph[curr]) {
+                inDegree[next]--;
+                if(inDegree[next]==0){
+                    qu.offer(next);
+                }
+            }
+        }
+
+        if (res.size() == n) {
+            System.out.println("Not Exists");
+        }else{
+            System.out.println("Exists");
+        }
+
+
+
+    }
+}
+
